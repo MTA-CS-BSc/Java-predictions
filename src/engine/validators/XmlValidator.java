11368539f35302593e01aa67f40ee7fe0509769e
Maya Raskin <mayaraskin14@gmail.com>
2023-08-01 17:17:39 +0300

    Implemented xml validator

--- src/engine/validators/XmlValidator.java
+++ src/engine/validators/XmlValidator.java
@@ -4,7 +4,6 @@ import engine.modules.Constants;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
-import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
@@ -13,19 +12,20 @@ import java.io.File;
 import java.io.IOException;
 
 public class XmlValidator {
-    static boolean testXml(String xmlPath) {
-        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+    static boolean validateXmlAgainstXsd(final String xmlPath) throws SAXException, IOException {
         try {
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
             Schema schema = schemaFactory.newSchema(new File(Constants.XSD_PATH));
             Validator validator = schema.newValidator();
             validator.validate(new StreamSource(new File(xmlPath)));
             return true;
-        } catch (SAXException e) {
-            // TODO: Handle exception
-            return false;
-        } catch (IOException e) {
-            // TODO: Handle exception
+        }
+
+        catch (Exception e) {
+            System.err.println("Validation failed: " + e.getMessage());
+            e.printStackTrace();
             return false;
         }
+
     }
 }
