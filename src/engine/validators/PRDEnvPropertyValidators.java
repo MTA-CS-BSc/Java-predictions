17928e46f179ece5ebf66e58ec25b52c4affc407
Maya Raskin <mayaraskin14@gmail.com>
2023-08-03 20:51:52 +0300

    Added PRDEnvProperty validators

--- src/engine/validators/PRDEnvPropertyValidators.java
+++ src/engine/validators/PRDEnvPropertyValidators.java
@@ -4,8 +4,29 @@ import engine.modules.Constants;
 import engine.prototypes.jaxb.PRDEnvProperty;
 import engine.prototypes.jaxb.PRDEvironment;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class PRDEnvPropertyValidators {
     public static boolean validatePropetyType(PRDEnvProperty property) {
         return Constants.PRD_ENV_PROPERTY_ALLOWED_TYPES.contains(property.getType());
+    }
+
+    public static boolean validateUniquePropertyName(PRDEvironment env, PRDEnvProperty property) {
+        return env.getPRDEnvProperty()
+                .stream()
+                .noneMatch(element -> element.getPRDName()
+                .equals(property.getPRDName()));
+    }
+
+    public static boolean validateRangeExistance(PRDEnvProperty property) {
+        List<String> allowedTypes = Arrays.asList("decimal", "float");
+
+        if (Objects.isNull(property.getPRDRange()))
+            return true;
+
+        return !Objects.isNull(property.getPRDRange())
+                && allowedTypes.contains(property.getType());
     }
 }
