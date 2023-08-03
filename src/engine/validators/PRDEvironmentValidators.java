a6cd98006a888ede5e5aba518e1494470b9eb133
Maya Raskin <mayaraskin14@gmail.com>
2023-08-03 21:22:58 +0300

    Added logs

--- src/engine/validators/PRDEvironmentValidators.java
+++ src/engine/validators/PRDEvironmentValidators.java
@@ -1,26 +1,56 @@
 package engine.validators;
 
+import engine.logs.Loggers;
+import engine.prototypes.jaxb.PRDEnvProperty;
 import engine.prototypes.jaxb.PRDEvironment;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class PRDEvironmentValidators {
     public static boolean validatePropsUniqueNames(PRDEvironment env) {
-        return env.getPRDEnvProperty()
-                .stream()
-                .allMatch(property -> PRDEnvPropertyValidators.validateUniquePropertyName(env, property));
+        for (PRDEnvProperty property : env.getPRDEnvProperty()) {
+            if (!PRDEnvPropertyValidators.validateUniquePropertyName(env, property)) {
+                Loggers.XML_ERRORS_LOGGER.error(String.format("Env property name [%s] already exists", property.getPRDName()));
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static boolean validatePropsTypes(PRDEvironment env) {
-        return env.getPRDEnvProperty()
-                .stream()
-                .allMatch(PRDEnvPropertyValidators::validatePropetyType);
+        boolean isValid = true;
+
+        for (PRDEnvProperty property : env.getPRDEnvProperty()) {
+            if (!PRDEnvPropertyValidators.validatePropetyType(property)) {
+                Loggers.XML_ERRORS_LOGGER.error(String.format("Env property [%s] has invalid type [%s]",
+                                                                property.getPRDName(), property.getType()));
+                isValid = false;
+            }
+        }
+
+        return isValid;
     }
 
     public static boolean validateRanges(PRDEvironment env) {
-        return env.getPRDEnvProperty()
-                .stream()
-                .filter(element -> !Objects.isNull(element.getPRDRange()))
-                .allMatch(PRDEnvPropertyValidators::validateTypeForRangeExistance);
+        boolean isValid = true;
+
+        List<PRDEnvProperty> propsWithRange =
+                                        env.getPRDEnvProperty()
+                                        .stream()
+                                        .filter(element -> !Objects.isNull(element.getPRDRange()))
+                                        .collect(Collectors.toList());
+
+        for (PRDEnvProperty property : propsWithRange) {
+            if (!PRDEnvPropertyValidators.validateTypeForRangeExistance(property)) {
+                Loggers.XML_ERRORS_LOGGER.error(String.format("Env property [%s] has range with incompatible type",
+                                                                  property.getPRDName()));
+                isValid = false;
+            }
+        }
+
+        return isValid;
     }
 }
