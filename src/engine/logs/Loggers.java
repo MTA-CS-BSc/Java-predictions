cfb76d876214c89e66f55b21308367ad691c02ce
Maya Raskin <mayaraskin14@gmail.com>
2023-08-03 21:22:44 +0300

    Added a logger for xml parser class

--- src/engine/logs/Loggers.java
+++ src/engine/logs/Loggers.java
@@ -2,9 +2,11 @@ package engine.logs;
 
 import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
+import engine.parsers.XmlParser;
 
 public class Loggers {
     public static final Logger ENGINE_LOGGER = LoggerFactory.getLogger(Loggers.class);
+    public static final Logger XML_ERRORS_LOGGER = LoggerFactory.getLogger(XmlParser.class);
 
     public static void ValueChangedLog(String context, String variableClassName) {
         Loggers.ENGINE_LOGGER.debug(String.format("[%s]: [%s] changed value", context, variableClassName));
