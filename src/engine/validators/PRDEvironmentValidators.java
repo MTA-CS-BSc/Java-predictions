ed6dc21282b891b49e13eb591c907159e4b7247b
Maya Raskin <mayaraskin14@gmail.com>
2023-08-03 20:52:23 +0300

    Added PRDEnvironment validators

---
+++ src/engine/validators/PRDEvironmentValidators.java
@@ -0,0 +1,26 @@
+package engine.validators;
+
+import engine.prototypes.jaxb.PRDEvironment;
+
+import java.util.Objects;
+
+public class PRDEvironmentValidators {
+    public static boolean validatePropsUniqueNames(PRDEvironment env) {
+        return env.getPRDEnvProperty()
+                .stream()
+                .allMatch(property -> PRDEnvPropertyValidators.validateUniquePropertyName(env, property));
+    }
+
+    public static boolean validatePropsTypes(PRDEvironment env) {
+        return env.getPRDEnvProperty()
+                .stream()
+                .allMatch(PRDEnvPropertyValidators::validatePropetyType);
+    }
+
+    public static boolean validateRanges(PRDEvironment env) {
+        return env.getPRDEnvProperty()
+                .stream()
+                .filter(element -> !Objects.isNull(element.getPRDRange()))
+                .allMatch(PRDEnvPropertyValidators::validateRangeExistance);
+    }
+}
