71e07a26049bef5e75c099e2ca14b1f6b87155d0
Maya Raskin <mayaraskin14@gmail.com>
2023-08-03 20:58:26 +0300

    Added a constant

--- src/engine/modules/Constants.java
+++ src/engine/modules/Constants.java
@@ -6,4 +6,6 @@ import java.util.List;
 public class Constants {
     public final static String XSD_PATH = "../schemas/predictions-v1.xsd";
     public final static List<String> PRD_ENV_PROPERTY_ALLOWED_TYPES = Arrays.asList("decimal", "float", "boolean", "string");
+
+    public final static List<String> PRD_ENV_PROPERTY_RANGE_ALLOWED_TYPES = Arrays.asList("decimal", "float");
 }
--- src/engine/validators/PRDEnvPropertyValidators.java
+++ src/engine/validators/PRDEnvPropertyValidators.java
@@ -4,8 +4,6 @@ import engine.modules.Constants;
 import engine.prototypes.jaxb.PRDEnvProperty;
 import engine.prototypes.jaxb.PRDEvironment;
 
-import java.util.Arrays;
-
 public class PRDEnvPropertyValidators {
     public static boolean validatePropetyType(PRDEnvProperty property) {
         return Constants.PRD_ENV_PROPERTY_ALLOWED_TYPES.contains(property.getType());
@@ -19,6 +17,6 @@ public class PRDEnvPropertyValidators {
     }
 
     public static boolean validateTypeForRangeExistance(PRDEnvProperty property) {
-        return Arrays.asList("decimal", "float").contains(property.getType());
+        return Constants.PRD_ENV_PROPERTY_RANGE_ALLOWED_TYPES.contains(property.getType());
     }
 }
