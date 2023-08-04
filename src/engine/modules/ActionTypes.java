689249890c5358a6fb9bb4cc17fa2db5e8dd6522
Maya Raskin <mayaraskin14@gmail.com>
2023-08-04 20:32:41 +0300

    Added some more constants classes

---
+++ src/engine/modules/ActionTypes.java
@@ -0,0 +1,10 @@
+package engine.modules;
+
+public class ActionTypes {
+    public static final String INCREASE = "increase";
+    public static final String DECREASE = "decrease";
+    public static final String CALCULATION = "calculation";
+    public static final String CONDITION = "condition";
+    public static final String SET = "set";
+    public static final String KILL = "kill";
+}
---
+++ src/engine/modules/SystemFunctions.java
@@ -0,0 +1,6 @@
+package engine.modules;
+
+public class SystemFunctions {
+    public static final String RANDOM = "random";
+    public static final String ENVIRONMENT = "environment";
+}
