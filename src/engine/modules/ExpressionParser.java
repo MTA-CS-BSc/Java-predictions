d159415782e0da71db4e591f6d22b3b0e4f46edc
Maya Raskin <mayaraskin14@gmail.com>
2023-08-04 21:07:12 +0300

    Finished implementing expression parser

--- src/engine/modules/ExpressionParser.java
+++ src/engine/modules/ExpressionParser.java
@@ -2,26 +2,41 @@ package engine.modules;
 
 import engine.prototypes.implemented.Entity;
 import engine.prototypes.implemented.World;
-import engine.prototypes.jaxb.PRDAction;
 import engine.validators.Utils;
 import engine.validators.actions.PRDActionValidators;
 
 import java.util.Objects;
 
 public class ExpressionParser {
-    public static Object parseExpression(World world, String entityName,
-                                         PRDAction relatedAction, String expression) {
+    public static Object parseSystemFunctionExpression(World world, String expression) {
+        String functionName = expression.substring(0, expression.indexOf("("));
+        String value = expression.substring(expression.indexOf("("), expression.lastIndexOf(")"));
+
+        if (functionName.equalsIgnoreCase(SystemFunctions.RANDOM)) {
+            try {
+                return RandomGenerator.randomizeRandomNumber(Integer.parseInt(value));
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        else if (functionName.equalsIgnoreCase(SystemFunctions.ENVIRONMENT))
+            return world.getEnvironment().getEnvVars().get(value);
+
+        return null;
+    }
+    public static Object parseExpression(World world, String entityName, String expression) {
         Entity entity = (Entity)Utils.findEntityByName(world, entityName);
 
         if (Objects.isNull(entity))
-            return "";
+            return null;
 
-        if (PRDActionValidators.isSystemFunction(expression))
-            return parseSystemFunctionExpression(expression);
+        if (PRDActionValidators.isSystemFunction(expression.substring(0, expression.indexOf("("))))
+            return parseSystemFunctionExpression(world, expression);
 
         else if (entity.getProperties().getPropsMap().containsKey(expression))
             return entity.getProperties().getPropsMap().get(expression);
 
-        return validateTypes(entity, relatedAction, expression);
+        return expression;
     }
 }
