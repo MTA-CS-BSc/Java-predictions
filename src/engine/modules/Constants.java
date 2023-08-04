5fa933e3b2219359f21ae3faa7811c88a21f573a
Maya Raskin <mayaraskin14@gmail.com>
2023-08-04 20:32:15 +0300

    reformatted

--- src/engine/modules/Constants.java
+++ src/engine/modules/Constants.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Constants {
     public final static String XSD_PATH = "../schemas/predictions-v1.xsd";
     public final static List<String> PRD_ENV_PROPERTY_ALLOWED_TYPES = Arrays.asList("decimal", "float", "boolean", "string");
-
     public final static List<String> PRD_ENV_PROPERTY_RANGE_ALLOWED_TYPES = Arrays.asList("decimal", "float");
     public final static List<String> CONDITION_ALLOWED_OPERATORS = Arrays.asList("=", "!=", "bt", "lt");
 
