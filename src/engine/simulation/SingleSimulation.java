6078b892a367cc3ef4ff3d40999e0d66e4fe1879
Maya Raskin <mayaraskin14@gmail.com>
2023-08-04 20:09:44 +0300

    Implemented a function to check if possible to apply rule

--- src/engine/simulation/SingleSimulation.java
+++ src/engine/simulation/SingleSimulation.java
@@ -1,13 +1,15 @@
 package engine.simulation;
 
 import engine.logs.Loggers;
-import engine.prototypes.jaxb.PRDBySecond;
-import engine.prototypes.jaxb.PRDByTicks;
-import engine.prototypes.jaxb.PRDTermination;
-import engine.prototypes.parsed.World;
+import engine.modules.RandomGenerator;
+import engine.prototypes.jaxb.*;
+import engine.prototypes.implemented.World;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class SingleSimulation {
     World world;
@@ -18,7 +20,7 @@ public class SingleSimulation {
         uuid = UUID.randomUUID();
         world = _world;
     }
-    public String isDone(long startTimeMillis) {
+    public String isSimulationFinished(long startTimeMillis) {
         PRDTermination termination = world.getTermination();
 
         for (Object stopCondition : termination.getPRDByTicksOrPRDBySecond()) {
@@ -33,14 +35,43 @@ public class SingleSimulation {
 
         return null;
     }
+    public boolean isPossibleToPerformRule(HashMap<String, PRDRule> allRules, String ruleName) {
+        float randomProbability = RandomGenerator.randomizeProbability();
+        PRDRule rule = allRules.get(ruleName);
+
+        return rule.getPRDActivation().getProbability() >= randomProbability
+                && rule.getPRDActivation().getTicks() % ticks == 0;
+    }
+    public HashMap<String, PRDRule> getRelevantRules() {
+        HashMap<String, PRDRule> allRules = world.getRules().getRulesMap();
+        List<String> relevantKeys = allRules.keySet()
+                                    .stream()
+                                    .filter(element -> isPossibleToPerformRule(allRules, element))
+                                    .collect(Collectors.toList());
+
+        HashMap<String, PRDRule> returned = new HashMap<>();
+
+        relevantKeys.forEach(key -> {
+           returned.put(key, allRules.get(key));
+        });
+
+        return returned;
+    }
+    public void handleSingleTick() {
+        HashMap<String, PRDRule> rulesToApply = getRelevantRules();
+
+        //Todo apply rules
+    }
+
     public void run() {
         long startTimeMillis = System.currentTimeMillis();
 
-        while (Objects.isNull(isDone(startTimeMillis))) {
-            // TODO: Implement run simulation
+        while (Objects.isNull(isSimulationFinished(startTimeMillis))) {
+            handleSingleTick();
+            ticks += 1;
         }
 
         Loggers.SIMULATION_LOGGER.info(String.format("Simulation [%s] ended due to [%s] condition reached",
-                                            uuid.toString(), isDone(startTimeMillis)));
+                                            uuid.toString(), isSimulationFinished(startTimeMillis)));
     }
 }
