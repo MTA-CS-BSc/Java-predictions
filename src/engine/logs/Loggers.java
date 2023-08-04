171ed6c35cb30bf92ebf5574fd4ab52ddce65117
Maya Raskin <mayaraskin14@gmail.com>
2023-08-04 19:39:56 +0300

    Added logger for single simulation

--- src/engine/logs/Loggers.java
+++ src/engine/logs/Loggers.java
@@ -1,11 +1,13 @@
 package engine.logs;
 
 import engine.parsers.XmlParser;
+import engine.simulation.SingleSimulation;
 
 import java.util.logging.Logger;
 
 public class Loggers {
     public static final Logger ENGINE_LOGGER = Logger.getLogger(Loggers.class.getSimpleName());
     public static final Logger XML_ERRORS_LOGGER = Logger.getLogger(XmlParser.class.getSimpleName());
+    public static final Logger SIMULATION_LOGGER = Logger.getLogger(SingleSimulation.class.getSimpleName());
 
 }
