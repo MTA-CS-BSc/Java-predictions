24e8cef14dc6f613103111a350c6280d9f8f2b1d
Maya Raskin <mayaraskin14@gmail.com>
2023-08-05 13:36:38 +0300

    Added some more constants

--- src/engine/modules/PropTypes.java
+++ src/engine/modules/PropTypes.java
@@ -1,8 +1,15 @@
 package engine.modules;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class PropTypes {
     public static final String DECIMAL = "decimal";
     public static final String BOOLEAN = "boolean";
     public static final String FLOAT = "float";
     public static final String STRING = "string";
+
+    public static final List<String> NUMERIC_PROPS = Arrays.asList(DECIMAL, FLOAT);
+    public static final List<String> BOOLEAN_PROPS = Collections.singletonList(BOOLEAN);
 }
