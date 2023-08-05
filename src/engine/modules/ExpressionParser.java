812c83698ae21d79425a346aea2a91c5813e5fb7
Maya Raskin <mayaraskin14@gmail.com>
2023-08-05 13:03:50 +0300

    Reimplemented ExpressionParser

--- src/engine/modules/ExpressionParser.java
+++ src/engine/modules/ExpressionParser.java
@@ -2,53 +2,53 @@ package engine.modules;
 
 import engine.prototypes.implemented.Entity;
 import engine.prototypes.implemented.World;
+import engine.prototypes.jaxb.PRDEntity;
+import engine.prototypes.jaxb.PRDProperty;
+import engine.prototypes.jaxb.PRDWorld;
 import engine.validators.Utils;
 
 import java.util.Arrays;
 import java.util.Objects;
 
 public class ExpressionParser {
-    public static Class<?> sameType(String obj1, String obj2) {
+
+    public static Object parseRandomSystemFunctionExpression(String value) {
         try {
-            Integer.parseInt(obj1);
-            Integer.parseInt(obj2);
-            return Integer.class;
-        }
-
-        catch (Exception e) {
-            try {
-                Float.parseFloat(obj1);
-                Float.parseFloat(obj2);
-                return Float.class;
-            }
-
-            catch (Exception er) {
-                return String.class;
-            }
+            return RandomGenerator.randomizeRandomNumber(Integer.parseInt(value));
+        } catch (Exception e) {
+            return null;
         }
     }
-    public static Object parseSystemFunctionExpression(World world, String expression) {
+    public static Object parseEnvSystemFunctionExpression(Object world, String functionName,
+                                                          String value) {
+        if (functionName.equalsIgnoreCase(SystemFunctions.ENVIRONMENT)
+                && world.getClass() == World.class)
+            return ((World)world).getEnvironment().getEnvVars().get(value);
+
+        else if (functionName.equalsIgnoreCase(SystemFunctions.ENVIRONMENT)
+                && world.getClass() == PRDWorld.class)
+            return ((PRDWorld)world).getPRDEvironment().getPRDEnvProperty()
+                    .stream().filter(element -> element.getPRDName().equals(value))
+                    .findFirst().orElse(null);
+
+        return null;
+    }
+    public static Object parseSystemFunctionExpression(Object world, String expression) {
         String functionName = expression.substring(0, expression.indexOf("("));
         String value = expression.substring(expression.indexOf("("), expression.lastIndexOf(")"));
 
-        if (functionName.equalsIgnoreCase(SystemFunctions.RANDOM)) {
-            try {
-                return RandomGenerator.randomizeRandomNumber(Integer.parseInt(value));
-            } catch (Exception e) {
-                return null;
-            }
-        }
+        if (functionName.equalsIgnoreCase(SystemFunctions.RANDOM))
+            return parseRandomSystemFunctionExpression(value);
 
-        else if (functionName.equalsIgnoreCase(SystemFunctions.ENVIRONMENT))
-            return world.getEnvironment().getEnvVars().get(value);
-
-        return null;
+        return parseEnvSystemFunctionExpression(world, functionName, value);
     }
     public static boolean isSystemFunction(String type) {
         return Arrays.asList(SystemFunctions.ENVIRONMENT, SystemFunctions.RANDOM).contains(type);
     }
-    public static Object parseExpression(World world, String entityName, String expression) {
-        Entity entity = (Entity)Utils.findEntityByName(world, entityName);
+
+    /* Returns PRDEnvProperty/PRDProperty/Property/int (if random)/String (default) */
+    public static Object parseExpression(Object world, String entityName, String expression) {
+        Object entity = Utils.findEntityByName(world, entityName);
 
         if (Objects.isNull(entity))
             return null;
@@ -57,8 +57,20 @@ public class ExpressionParser {
                 && isSystemFunction(expression.substring(0, expression.indexOf("("))))
             return parseSystemFunctionExpression(world, expression);
 
-        else if (entity.getProperties().getPropsMap().containsKey(expression))
-            return entity.getProperties().getPropsMap().get(expression);
+        if (entity.getClass() == Entity.class) {
+            if (((Entity)entity).getProperties().getPropsMap().containsKey(expression))
+                return (((Entity)entity).getProperties().getPropsMap().get(expression));
+        }
+
+        else if (entity.getClass() == PRDEntity.class) {
+            PRDProperty prop = (((PRDEntity)entity).getPRDProperties().getPRDProperty()
+                    .stream()
+                    .filter(element -> element.getPRDName().equals(expression))
+                    .findAny().orElse(null));
+
+            if (!Objects.isNull(prop))
+                return prop;
+        }
 
         return expression;
     }
