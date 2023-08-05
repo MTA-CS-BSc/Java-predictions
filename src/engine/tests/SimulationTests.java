16c40d1dd97505d76fd05c839832a5e348b1ffd2
Maya Raskin <mayaraskin14@gmail.com>
2023-08-05 20:02:07 +0300

    Changed test

--- src/engine/tests/SimulationTests.java
+++ src/engine/tests/SimulationTests.java
@@ -1,12 +1,10 @@
 package engine.tests;
 
 import engine.logs.Loggers;
-import engine.modules.TerminationReasons;
 import engine.prototypes.implemented.World;
 import engine.prototypes.jaxb.PRDWorld;
 import engine.simulation.SingleSimulation;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 
 import javax.xml.bind.JAXBException;
@@ -28,6 +26,6 @@ public class SimulationTests {
         PRDWorld prdWorld = parserTests.validationsForValidXml();
         World world = new World(prdWorld);
         SingleSimulation simulation = new SingleSimulation(world);
-        Assertions.assertEquals(simulation.run(), TerminationReasons.BY_TICKS);
+        simulation.run();
     }
 }
