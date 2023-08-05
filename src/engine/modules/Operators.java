ebc8c73ed033da609b70c8322a5ce7b5017d144b
Maya Raskin <mayaraskin14@gmail.com>
2023-08-05 13:02:43 +0300

    Added PropTypes & Operators constants

---
+++ src/engine/modules/Operators.java
@@ -0,0 +1,8 @@
+package engine.modules;
+
+public class Operators {
+    public static final String BT = "bt";
+    public static final String LT = "lt";
+    public static final String EQUALS = "=";
+    public static final String NOT_EQUALS = "!=";
+}
---
+++ src/engine/modules/PropTypes.java
@@ -0,0 +1,8 @@
+package engine.modules;
+
+public class PropTypes {
+    public static final String DECIMAL = "decimal";
+    public static final String BOOLEAN = "boolean";
+    public static final String FLOAT = "float";
+    public static final String STRING = "string";
+}
