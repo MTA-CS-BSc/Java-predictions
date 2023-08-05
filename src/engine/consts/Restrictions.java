4f9190c43b5ce2b3634cc62b9c32680bc50f3038
Maya Raskin <mayaraskin14@gmail.com>
2023-08-06 00:22:55 +0300

    Changed module name

--- src/engine/consts/Restrictions.java
+++ src/engine/consts/Restrictions.java
@@ -8,12 +8,8 @@ public class Restrictions {
     public final static List<String> PRD_ENV_PROPERTY_ALLOWED_TYPES = Arrays.asList(PropTypes.DECIMAL,
             PropTypes.FLOAT, PropTypes.BOOLEAN, PropTypes.STRING);
     public final static List<String> PRD_ENV_PROPERTY_RANGE_ALLOWED_TYPES = Arrays.asList(PropTypes.DECIMAL, PropTypes.FLOAT);
-    public final static List<String> CONDITION_ALLOWED_OPERATORS = Arrays.asList(Operators.EQUALS, Operators.NOT_EQUALS,
-            Operators.BT, Operators.LT);
-
     public final static int MAX_RANDOM_STRING_LENGTH = 50;
 
     public final static int MIN_RANGE = -999999999;
     public final static int MAX_RANGE = 999999999;
-
 }
