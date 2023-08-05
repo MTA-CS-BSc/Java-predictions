675ba969079de7359e33dacec3bd21a4576f2b54
Maya Raskin <mayaraskin14@gmail.com>
2023-08-05 16:38:31 +0300

    added ex2 action consts

--- src/engine/modules/ActionTypes.java
+++ src/engine/modules/ActionTypes.java
@@ -11,6 +11,8 @@ public class ActionTypes {
     public static final String CONDITION = "condition";
     public static final String SET = "set";
     public static final String KILL = "kill";
+    public static final String REPLACE = "replace";
+    public static final String PROXIMITY = "proximity";
     public static List<String> NUMERIC_ACTIONS = Arrays.asList(INCREASE, DECREASE, CALCULATION);
     public static List<String> BOOLEAN_ACTIONS = Collections.singletonList(CONDITION);
 }
