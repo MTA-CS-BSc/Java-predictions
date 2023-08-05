d87623547524f9aa4bfc75b1ccff138cf34bd57b
Maya Raskin <mayaraskin14@gmail.com>
2023-08-05 16:14:17 +0300

    Added assignment2 system functions

--- src/engine/modules/SystemFunctions.java
+++ src/engine/modules/SystemFunctions.java
@@ -3,4 +3,6 @@ package engine.modules;
 public class SystemFunctions {
     public static final String RANDOM = "random";
     public static final String ENVIRONMENT = "environment";
+    public static final String EVALUATE = "evaluate";
+    public static final String PRECENT = "precent";
 }
