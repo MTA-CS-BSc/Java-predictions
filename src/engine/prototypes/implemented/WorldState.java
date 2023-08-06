65ea6a583687d4ced4a7a1f66ef01e6a5d891714
Maya Raskin <mayaraskin14@gmail.com>
2023-08-07 01:47:09 +0300

    Added world state class

---
+++ src/engine/prototypes/implemented/WorldState.java
@@ -0,0 +1,11 @@
+package engine.prototypes.implemented;
+
+import java.util.HashMap;
+
+public class WorldState {
+    HashMap<String, Entity> entitiesMap;
+    public WorldState(World world) {
+        entitiesMap = new HashMap<>();
+        world.getEntities().entitiesMap.forEach((key, entity) -> entitiesMap.put(key, new Entity(entity)));
+    }
+}
