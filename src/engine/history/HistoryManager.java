6a6c985a5647d05c4685d5fc9d88c5246fbd9d05
Maya Raskin <mayaraskin14@gmail.com>
2023-08-07 10:54:01 +0300

    Implemented getEntitiesBeforeAfter on HistoryManager

--- src/engine/history/HistoryManager.java
+++ src/engine/history/HistoryManager.java
@@ -1,8 +1,12 @@
 package engine.history;
 
+import engine.logs.Loggers;
+import engine.modules.Utils;
+import engine.prototypes.implemented.Entity;
 import engine.simulation.SingleSimulationLog;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class HistoryManager {
     protected HashMap<String, SingleSimulationLog> pastSimulations;
@@ -17,5 +21,30 @@ public class HistoryManager {
         return pastSimulations.get(uuid);
     }
 
-    //TODO: Implement requested funcs
+    public void getSimulationDetails(String uuid, SingleSimulationLog simulationLog) {
+        if (Objects.isNull(simulationLog)) {
+            Loggers.HISTORY_LOGGER.info(String.format("No simulation found with uuid [%s]", uuid));
+            return;
+        }
+
+        System.out.printf("Simulation uuid: [%s]%n", uuid);
+        System.out.printf("Simulation start time:  [%s]%n", Utils.formatDate(simulationLog.getStartTime()));
+    }
+    public HashMap<String, Integer[]> getEntitiesBeforeAndAfter(String uuid) {
+        HashMap<String, Integer[]> entitiesBeforeAfter = new HashMap<>();
+
+        SingleSimulationLog simulationLog = getPastSimulation(uuid);
+        getSimulationDetails(uuid, simulationLog);
+
+        simulationLog.getStartWorldState().getEntitiesMap().forEach((key, entity) -> {
+            int startAmount = entity.getPopulation();
+            Entity finishEntity = simulationLog.getFinishWorldState().getEntitiesMap().get(key);
+            int finishAmount =  Objects.isNull(finishEntity) ? 0 : finishEntity.getPopulation();
+            Integer[] array = { startAmount, finishAmount };
+
+            entitiesBeforeAfter.put(key, array);
+        });
+
+        return entitiesBeforeAfter;
+    }
 }
