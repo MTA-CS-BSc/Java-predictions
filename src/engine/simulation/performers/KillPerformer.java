310fbe474f76f5e38dd236ec7862f1d57e6b7a49
Maya Raskin <mayaraskin14@gmail.com>
2023-08-07 16:09:58 +0300

    Added logs to kill performer

--- src/engine/simulation/performers/KillPerformer.java
+++ src/engine/simulation/performers/KillPerformer.java
@@ -1,20 +1,31 @@
 package engine.simulation.performers;
 
+import engine.exceptions.EntityNotFoundException;
+import engine.exceptions.ErrorMessageFormatter;
 import engine.logs.Loggers;
 import engine.modules.Utils;
 import engine.parsers.ExpressionParser;
 import engine.prototypes.implemented.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class KillPerformer {
-    public static void handleAll(World world, String entityName) {
-        world.getEntities().getEntitiesMap().remove(entityName);
+    public static void handleAll(World world, Action action) {
+        Entity mainEntity = Utils.findEntityByName(world, action.getEntityName());
+
+        mainEntity.setPopulation(0);
+        mainEntity.setSingleEntities(Collections.emptyList());
+
+        Loggers.SIMULATION_LOGGER.info(String.format("Killed entity [%s]", action.getEntityName()));
     }
-    public static void handleSingle(Entity mainEntity, SingleEntity kill) {
+
+    public static void handleSingle(World world, Action action, SingleEntity kill) {
+        Entity mainEntity = Utils.findEntityByName(world, action.getEntityName());
+
         List<SingleEntity> updatedList = mainEntity.getSingleEntities()
                                         .stream()
                                         .filter(element -> !element.equals(kill))
@@ -22,16 +33,17 @@ public class KillPerformer {
 
         mainEntity.setSingleEntities(updatedList);
         mainEntity.setPopulation(mainEntity.getPopulation() - 1);
+
+        Loggers.SIMULATION_LOGGER.info(String.format("Killed 1 entity named [%s]. Population is [%d]", action.getEntityName(), mainEntity.getPopulation()));
     }
-    public static void handle(World world, Action action, SingleEntity kill) {
-        Entity mainEntity = Utils.findEntityByName(world, action.getEntityName());
+    public static void handle(World world, Action action, SingleEntity kill) throws EntityNotFoundException {
+        if (Objects.isNull(Utils.findEntityByName(world, action.getEntityName())))
+            throw new EntityNotFoundException(ErrorMessageFormatter.formatEntityNotFoundMessage(action.getType(), action.getEntityName()));
 
         if (Objects.isNull(kill))
-            handleAll(world, action.getEntityName());
+            handleAll(world, action);
 
         else
-            handleSingle(mainEntity, kill);
-
-        Loggers.SIMULATION_LOGGER.info("Kill evaluate");
+            handleSingle(world, action, kill);
     }
 }
