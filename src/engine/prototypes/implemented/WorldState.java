ad0539dfefc406c2211b4a627bf0a95341ff8fa2
Maya Raskin <mayaraskin14@gmail.com>
2023-08-07 10:53:39 +0300

    added entitiesMap getter

--- src/engine/prototypes/implemented/WorldState.java
+++ src/engine/prototypes/implemented/WorldState.java
@@ -4,8 +4,13 @@ import java.util.HashMap;
 
 public class WorldState {
     HashMap<String, Entity> entitiesMap;
+
     public WorldState(World world) {
         entitiesMap = new HashMap<>();
         world.getEntities().entitiesMap.forEach((key, entity) -> entitiesMap.put(key, new Entity(entity)));
+    }
+
+    public HashMap<String, Entity> getEntitiesMap() {
+        return entitiesMap;
     }
 }
