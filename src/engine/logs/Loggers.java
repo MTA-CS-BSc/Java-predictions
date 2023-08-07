c9b63d9748bd4e977594cef092c5c7c47bad7c09
Maya Raskin <mayaraskin14@gmail.com>
2023-08-07 10:53:23 +0300

    added history logger

--- src/engine/logs/Loggers.java
+++ src/engine/logs/Loggers.java
@@ -1,5 +1,6 @@
 package engine.logs;
 
+import engine.history.HistoryManager;
 import engine.parsers.XmlParser;
 import engine.simulation.SingleSimulation;
 
@@ -8,5 +9,5 @@ import java.util.logging.Logger;
 public class Loggers {
     public static final Logger XML_ERRORS_LOGGER = Logger.getLogger(XmlParser.class.getSimpleName());
     public static final Logger SIMULATION_LOGGER = Logger.getLogger(SingleSimulation.class.getSimpleName());
-
+    public static final Logger HISTORY_LOGGER = Logger.getLogger(HistoryManager.class.getSimpleName());
 }
