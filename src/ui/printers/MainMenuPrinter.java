a4984d3e5dba3eed8df9f2ef16fc9657dbe1ffa6
Maya Raskin <mayaraskin14@gmail.com>
2023-08-09 23:58:45 +0300

    added main menu printer

---
+++ src/ui/printers/MainMenuPrinter.java
@@ -0,0 +1,19 @@
+package ui.printers;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+import ui.enums.MainMenu;
+
+import java.util.Arrays;
+
+public class MainMenuPrinter {
+    public static void print() {
+        System.out.println("Please choose an option from the menu below:");
+        System.out.println("---------------------------------------------");
+
+        Arrays.stream(MainMenu.values())
+                .forEach(element -> {
+                    System.out.printf("%d -> %s\n", element.ordinal() + 1,
+                            StringUtils.capitalize(element.name().replace("_", " ").toLowerCase()));
+                });
+    }
+}
