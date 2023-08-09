b87d4d950173c31f9ec99186e28350f3d105ee5a
Maya Raskin <mayaraskin14@gmail.com>
2023-08-10 00:32:43 +0300

    added handler

---
+++ src/ui/handlers/MainMenuHandler.java
@@ -0,0 +1,26 @@
+package ui.handlers;
+
+
+import ui.consts.Constants;
+import ui.enums.MainMenu;
+import ui.printers.MainMenuPrinter;
+import ui.scanners.MainMenuScanner;
+
+public class MainMenuHandler extends MainMenuScanner {
+    public MainMenuHandler() {
+        super();
+    }
+
+    public int selectionCycle() {
+        MainMenuPrinter.print();
+        int selected = scanMenuOption();
+
+        while (selected != MainMenu.EXIT.ordinal() + 1  && selected != Constants.NOT_FOUND) {
+            System.out.println("Invalid choice! Please try again");
+            MainMenuPrinter.print();
+            selected = scanMenuOption();
+        }
+
+        return selected;
+    }
+}
