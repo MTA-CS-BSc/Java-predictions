2fdd6a4bd902ce0b19f4bcd78bbe52bc73b53132
Maya Raskin <mayaraskin14@gmail.com>
2023-08-10 00:08:28 +0300

    removed redundant logger

--- src/engine/logs/Loggers.java
+++ src/engine/logs/Loggers.java
@@ -9,5 +9,4 @@ import java.util.logging.Logger;
 public class Loggers {
     public static final Logger XML_ERRORS_LOGGER = Logger.getLogger(XmlParser.class.getSimpleName());
     public static final Logger SIMULATION_LOGGER = Logger.getLogger(SingleSimulation.class.getSimpleName());
-    public static final Logger HISTORY_LOGGER = Logger.getLogger(HistoryManager.class.getSimpleName());
 }
