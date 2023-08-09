60b138c2cbfe2792293cd5b132585aeb34b8148e
Maya Raskin <mayaraskin14@gmail.com>
2023-08-10 00:32:12 +0300

    added loggers

---
+++ src/ui/logs/UILoggers.java
@@ -0,0 +1,9 @@
+package ui.logs;
+
+import ui.scanners.MainMenuScanner;
+
+import java.util.logging.Logger;
+
+public class UILoggers {
+    public static final Logger MainMenuScannerLogger = Logger.getLogger(MainMenuScanner.class.getSimpleName());
+}
