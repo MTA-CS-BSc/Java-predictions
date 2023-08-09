d11d9265cf37430136ff6f5a6baf32dbb4b26209
Maya Raskin <mayaraskin14@gmail.com>
2023-08-09 12:12:27 +0300

    Added evaluateSystemExpression to an external function

--- src/engine/parsers/ExpressionParser.java
+++ src/engine/parsers/ExpressionParser.java
@@ -18,29 +18,34 @@ public class ExpressionParser {
         Property expressionEntityProp = Objects.isNull(on) ? Utils.findAnyPropertyByName(world, action.getEntityName(), expression)
                 : Utils.findPropertyByName(on, expression);
 
-        if (ValidatorsUtils.isSystemFunction(expression)) {
-            String systemFunctionType = ValidatorsUtils.getSystemFunctionType(expression);
-
-            if (systemFunctionType.equals(SystemFunctions.RANDOM))
-                return evaluateSystemFuncRandom(expression);
-
-            else if (systemFunctionType.equals(SystemFunctions.ENVIRONMENT))
-                return evaluateSystemFuncEnv(world, expression.substring(expression.lastIndexOf("(") + 1,
-                        expression.lastIndexOf(")")));
-        }
+        if (ValidatorsUtils.isSystemFunction(expression))
+            return evaluateSystemExpression(world, action, expression, on);
 
         else if (!Objects.isNull(expressionEntityProp))
             return expressionEntityProp.getValue().getCurrentValue();
 
         return expression;
     }
-    public static String evaluateSystemFuncRandom(String expression) {
+
+    private static String evaluateSystemExpression(World world, Action action, String expression, SingleEntity on) throws PropertyNotFoundException {
+        String systemFunctionType = ValidatorsUtils.getSystemFunctionType(expression);
+
+        if (systemFunctionType.equals(SystemFunctions.RANDOM))
+            return evaluateSystemFuncRandom(expression);
+
+        else if (systemFunctionType.equals(SystemFunctions.ENVIRONMENT))
+            return evaluateSystemFuncEnv(world, expression.substring(expression.lastIndexOf("(") + 1,
+                    expression.lastIndexOf(")")));
+
+        return "";
+    }
+    private static String evaluateSystemFuncRandom(String expression) {
         String bound = expression.substring(expression.lastIndexOf("(") + 1,
                 expression.lastIndexOf(")"));
 
         return String.valueOf(RandomGenerator.randomizeRandomNumber(0, Integer.parseInt(bound)));
     }
-    public static String evaluateSystemFuncEnv(World world, String propertyName) throws PropertyNotFoundException {
+    private static String evaluateSystemFuncEnv(World world, String propertyName) throws PropertyNotFoundException {
         Property envProp = world.getEnvironment().getEnvVars().values()
                 .stream()
                 .filter(element -> element.getName().equals(propertyName))
