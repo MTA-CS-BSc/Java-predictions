302c3b6813a5de9d55f803ad6ab2a9dd87ff9609
Maya Raskin <mayaraskin14@gmail.com>
2023-08-11 01:04:05 +0300

    reformatted

--- src/ui/scanners/MainMenuScanner.java
+++ src/ui/scanners/MainMenuScanner.java
@@ -1,8 +1,6 @@
 package ui.scanners;
 
-import ui.consts.Constants;
 import ui.enums.MainMenu;
-import ui.logs.UILoggers;
 
 import java.util.Scanner;
 
@@ -13,24 +11,5 @@ public abstract class MainMenuScanner {
     public MainMenuScanner() {
         maxOption = MainMenu.values().length;
         scanner = new Scanner(System.in);
-    }
-    protected int scanMenuOption() {
-        int selected;
-
-        try {
-            selected = scanner.nextInt();
-
-            if (selected > maxOption || selected < 1) {
-                UILoggers.ScannerLogger.info("Option value entered is not in range");
-                return Constants.NOT_FOUND;
-            }
-
-            return selected;
-        }
-
-        catch (Exception e) {
-            UILoggers.ScannerLogger.info("Option value entered is not a number");
-            return Constants.NOT_FOUND;
-        }
     }
 }
