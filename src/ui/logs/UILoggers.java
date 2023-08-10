dfbffd94e7ea036818984cc95701ec03d2eed0e9
Maya Raskin <mayaraskin14@gmail.com>
2023-08-10 22:51:05 +0300

    added a logger

--- src/ui/logs/UILoggers.java
+++ src/ui/logs/UILoggers.java
@@ -1,5 +1,6 @@
 package ui.logs;
 
+import ui.handlers.Orchestrator;
 import ui.scanners.MainMenuScanner;
 
 import java.util.Scanner;
@@ -7,4 +8,5 @@ import java.util.logging.Logger;
 
 public class UILoggers {
     public static final Logger ScannerLogger = Logger.getLogger(Scanner.class.getSimpleName());
+    public static final Logger OrchestratorLogger = Logger.getLogger(Orchestrator.class.getSimpleName());
 }
