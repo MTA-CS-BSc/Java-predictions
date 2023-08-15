e575e7289ce08771f26fe80391abb9f48e4c1056
Maya Raskin <mayaraskin14@gmail.com>
2023-08-15 23:39:45 +0300

    Added validation stop condition exists

--- src/engine/validators/termination/PRDTerminationValidators.java
+++ src/engine/validators/termination/PRDTerminationValidators.java
@@ -1,11 +1,13 @@
 package engine.validators.termination;
 
-import engine.prototypes.jaxb.PRDTermination;
 import engine.prototypes.jaxb.PRDWorld;
+
+import java.util.Objects;
 
 public abstract class PRDTerminationValidators {
     public static boolean validateStopConditionExists(PRDWorld world) throws Exception {
-        if (world.getPRDTermination().getPRDByTicksOrPRDBySecond().isEmpty())
+        if (world.getPRDTermination().getPRDBySecondOrPRDByTicks().isEmpty()
+        && Objects.isNull(world.getPRDTermination().getPRDByUser()))
             throw new Exception("No termination found");
 
         return true;
