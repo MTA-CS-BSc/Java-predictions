ed2cf43e3bdc0ba04ce1808689f52edce0d858ab
Maya Raskin <mayaraskin14@gmail.com>
2023-08-15 13:51:42 +0300

    added start timestamp on SingleSimulationDTO

--- src/engine/simulation/SingleSimulationDTO.java
+++ src/engine/simulation/SingleSimulationDTO.java
@@ -4,12 +4,17 @@ import engine.prototypes.implemented.World;
 
 public class SingleSimulationDTO {
     protected final String uuid;
+    protected final String startTimestamp;
     private final World world;
 
     public SingleSimulationDTO(SingleSimulation simulation) {
         uuid = simulation.getUUID();
+        startTimestamp = simulation.getStartTimestamp();
         world = simulation.getWorld();
     }
+    public String getUuid() { return uuid; }
+
+    public String getStartTimestamp() { return startTimestamp; }
     @Override
     public String toString() {
         return "--------------------------------------\n" +
