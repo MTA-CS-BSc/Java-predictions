228a27787b377818e1061736b2fce4a0e09a7d2d
Maya Raskin <mayaraskin14@gmail.com>
2023-08-15 14:48:09 +0300

    Added EntityDTO

---
+++ src/engine/prototypes/EntityDTO.java
@@ -0,0 +1,23 @@
+package engine.prototypes;
+
+import engine.prototypes.implemented.Entity;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EntityDTO {
+    protected String name;
+    protected List<PropertyDTO> properties;
+
+    public EntityDTO(Entity entity) {
+        name = entity.getName();
+        properties = entity.getInitialProperties().getPropsMap().values()
+                .stream()
+                .map(PropertyDTO::new)
+                .sorted(Comparator.comparing(PropertyDTO::getName))
+                .collect(Collectors.toList());
+    }
+
+    public String getName() { return name; }
+}
