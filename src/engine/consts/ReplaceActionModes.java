4b21d475a2db4ed83d53f7e59f6c8fb8c356a5cc
Maya Raskin <mayaraskin14@gmail.com>
2023-08-16 13:21:05 +0300

    Added replace action validations

---
+++ src/engine/consts/ReplaceActionModes.java
@@ -0,0 +1,10 @@
+package engine.consts;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReplaceActionModes {
+    public static final String SCRATCH = "scratch";
+    public static final String DERIVED = "derived";
+    public static final List<String> REPLACE_MODES = Arrays.asList(SCRATCH, DERIVED);
+}
