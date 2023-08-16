313516595dc932a1fef600b023640a4ac7bec08e
Maya Raskin <mayaraskin14@gmail.com>
2023-08-16 21:36:51 +0300

    added comments

--- src/ui/logs/UILoggers.java
+++ src/ui/logs/UILoggers.java
@@ -1,11 +1,11 @@
 package ui.logs;
 
 import ui.handlers.Orchestrator;
-import ui.scanners.MainMenuScanner;
 
 import java.util.Scanner;
 import java.util.logging.Logger;
 
+// Using JUL because in this project, a logging framework is a bit of an overkill.
 public class UILoggers {
     public static final Logger ScannerLogger = Logger.getLogger(Scanner.class.getSimpleName());
     public static final Logger OrchestratorLogger = Logger.getLogger(Orchestrator.class.getSimpleName());
