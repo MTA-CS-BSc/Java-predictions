7c801d265f449729e70588d6010361c5a7373459
Maya Raskin <mayaraskin14@gmail.com>
2023-08-19 17:56:41 +0300

    Updated all actions exceptions

--- src/engine/simulation/performers/ActionsPerformer.java
+++ src/engine/simulation/performers/ActionsPerformer.java
@@ -3,9 +3,7 @@ package engine.simulation.performers;
 import engine.consts.ActionTypes;
 import engine.consts.PropTypes;
 import engine.exceptions.EntityNotFoundException;
-import engine.exceptions.ErrorMessageFormatter;
 import engine.exceptions.InvalidTypeException;
-import engine.exceptions.ValueNotInRangeException;
 import engine.logs.EngineLoggers;
 import engine.modules.Utils;
 import engine.prototypes.implemented.*;
@@ -71,14 +69,16 @@ public abstract class ActionsPerformer {
                                          Property property, String newValue) throws Exception {
         newValue = Utils.removeExtraZeroes(property, newValue);
 
-        if (TypesUtils.isFloat(newValue) && !TypesUtils.isDecimal(newValue) && property.getType().equals(PropTypes.DECIMAL))
+        if (TypesUtils.isFloat(newValue) && !TypesUtils.isDecimal(newValue) && property.getType().equals(PropTypes.DECIMAL)) {
             throw new InvalidTypeException(String.format("Action [%s]: Entity: [%s]: Property [%s]: [%s] is not decimal and therefore is not set",
                     actionType, entityName, property.getName(), newValue));
+        }
 
-        if (!Utils.validateValueInRange(property, newValue))
-            throw new ValueNotInRangeException(ErrorMessageFormatter.formatActionErrorMessage(
-                    actionType, entityName, property.getName(),
-                    String.format("value [%s] not in range and therefore is not set", newValue)));
+        //TODO: Change
+//        if (!Utils.validateValueInRange(property, newValue))
+//            throw new ValueNotInRangeException(ErrorMessageFormatter.formatActionErrorMessage(
+//                    actionType, entityName, property.getName(),
+//                    String.format("value [%s] not in range and therefore is not set", newValue)));
 
         property.getValue().setCurrentValue(newValue);
         property.setStableTime(0);
--- src/engine/simulation/performers/DecrementPerformer.java
+++ src/engine/simulation/performers/DecrementPerformer.java
@@ -3,7 +3,6 @@ package engine.simulation.performers;
 import engine.consts.ActionTypes;
 import engine.exceptions.ErrorMessageFormatter;
 import engine.exceptions.PropertyNotFoundException;
-import engine.logs.EngineLoggers;
 import engine.modules.Utils;
 import engine.parsers.ExpressionParser;
 import engine.prototypes.implemented.*;
@@ -17,16 +16,11 @@ public abstract class DecrementPerformer {
 
         return result.matches(Constants.REGEX_ONLY_ZEROES_AFTER_DOT) ? result.split("\\.")[0] : result;
     }
-    private static void handleAll(World world, Action action) {
+    private static void handleAll(World world, Action action) throws Exception {
         Entity mainEntity = Utils.findEntityByName(world, action.getEntityName());
 
-        mainEntity.getSingleEntities().forEach(entity -> {
-            try {
-                handleSingle(world, action, entity);
-            } catch (Exception e) {
-                EngineLoggers.SIMULATION_LOGGER.info(e.getMessage());
-            }
-        });
+        for (SingleEntity entity : mainEntity.getSingleEntities())
+            handleSingle(world, action, entity);
     }
     private static void handleSingle(World world, Action action, SingleEntity on) throws Exception {
         Property propToChange = Utils.findPropertyByName(on, action.getPropertyName());
@@ -34,19 +28,14 @@ public abstract class DecrementPerformer {
         String newValue = getDecrementResult(propToChange.getValue().getCurrentValue(), by);
         ActionsPerformer.setPropertyValue(ActionTypes.DECREASE, action.getEntityName(), propToChange, newValue);
     }
-    private static void performAction(World world, Action action, SingleEntity on) {
+    private static void performAction(World world, Action action, SingleEntity on) throws Exception {
         if (Objects.isNull(on))
             handleAll(world, action);
 
-        else {
-            try {
-                handleSingle(world, action, on);
-            } catch (Exception e) {
-                EngineLoggers.SIMULATION_LOGGER.info(e.getMessage());
-            }
-        }
+        else
+            handleSingle(world, action, on);
     }
-    public static void handle(World world, Action action, SingleEntity on) throws PropertyNotFoundException {
+    public static void handle(World world, Action action, SingleEntity on) throws Exception {
         if (Objects.isNull(Utils.findAnyPropertyByName(world, action.getEntityName(), action.getPropertyName())))
             throw new PropertyNotFoundException(ErrorMessageFormatter.formatPropertyNotFoundMessage(action.getType(), action.getEntityName(), action.getPropertyName()));
 
