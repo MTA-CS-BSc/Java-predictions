00db30edf27aac3721401472040e5733321a89a5
Maya Raskin <mayaraskin14@gmail.com>
2023-08-28 15:04:41 +0300

    Added grid max min constants

--- src/helpers/Constants.java
+++ src/helpers/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static final String REGEX_ONLY_ZEROES_AFTER_DOT = "^\\d+\\.0+$";
     public final static int MIN_RANGE = -999999999;
     public final static int MAX_RANGE = 999999999;
+    public final static int MIN_GRID_HEIGHT = 10;
+    public final static int MAX_GRID_HEIGHT = 100;
 }
