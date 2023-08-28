476fecad633d83208e0fdfa0853b094e58854489
Maya Raskin <mayaraskin14@gmail.com>
2023-08-28 15:04:13 +0300

    removed redundant variable

--- src/engine/consts/Restrictions.java
+++ src/engine/consts/Restrictions.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public abstract class Restrictions {
-    public final static String XSD_PATH = "../schemas/predictions-v1.xsd";
     public final static List<String> PRD_PROPERTY_ALLOWED_TYPES = Arrays.asList(PropTypes.DECIMAL,
             PropTypes.FLOAT, PropTypes.BOOLEAN, PropTypes.STRING);
     public final static List<String> PRD_PROPERTY_RANGE_ALLOWED_TYPES = Arrays.asList(PropTypes.DECIMAL, PropTypes.FLOAT);
