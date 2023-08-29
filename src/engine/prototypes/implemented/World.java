3d8f982ca570978af05c660eda763fe7b51ac7e0
Maya Raskin <mayaraskin14@gmail.com>
2023-08-29 15:23:15 +0300

    Added grid initialization on World ctor

--- src/engine/prototypes/implemented/World.java
+++ src/engine/prototypes/implemented/World.java
@@ -11,17 +11,20 @@ public class World implements Serializable {
     protected Entities entities;
     protected Termination termination;
     protected Rules rules;
+    protected WorldGrid grid;
 
     public World(PRDWorld world) {
         rules = new Rules(world.getPRDRules().getPRDRule());
         entities = new Entities(world.getPRDEntities().getPRDEntity());
         environment = new Environment(world.getPRDEnvironment().getPRDEnvProperty());
         termination = new Termination(world.getPRDTermination());
+        grid = new WorldGrid(world.getPRDGrid());
     }
     public Environment getEnvironment() { return environment; }
     public Entities getEntities() { return entities; }
     public Termination getTermination() { return termination; }
     public Rules getRules() { return rules; }
+    public WorldGrid getGrid() { return grid; }
     public void initAllRandomVars() {
         entities.getEntitiesMap().values().forEach(entity -> {
             entity.getSingleEntities().forEach(singleEntity -> {
