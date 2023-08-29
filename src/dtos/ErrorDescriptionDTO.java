ca8df6c8fbfaa5fc902df82aee4027fdfcc3b2bf
Maya Raskin <mayaraskin14@gmail.com>
2023-08-29 13:44:26 +0300

    Changed ErrorDescriptionDTO

--- src/dtos/ErrorDescriptionDTO.java
+++ src/dtos/ErrorDescriptionDTO.java
@@ -2,10 +2,8 @@ package dtos;
 
 public class ErrorDescriptionDTO {
     protected String cause;
-    protected String exceptionType;
-
-    public ErrorDescriptionDTO(String _cause, String _exceptionType) {
+    public ErrorDescriptionDTO(String _cause) {
         cause = _cause;
-        exceptionType = _exceptionType;
     }
+    public String getCause() { return cause; }
 }
