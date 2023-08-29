8726d7ae8050044630cf5ea5ba23ef17bc73710a
Maya Raskin <mayaraskin14@gmail.com>
2023-08-29 22:49:30 +0300

    Implemented ProximityPerformer

--- src/engine/simulation/performers/ProximityPerformer.java
+++ src/engine/simulation/performers/ProximityPerformer.java
@@ -1,11 +1,36 @@
 package engine.simulation.performers;
 
-import engine.prototypes.implemented.Action;
-import engine.prototypes.implemented.SingleEntity;
-import engine.prototypes.implemented.World;
+import engine.modules.Utils;
+import engine.parsers.ExpressionParser;
+import engine.prototypes.implemented.*;
+
+import java.util.Objects;
 
 public abstract class ProximityPerformer {
-    public static void handle(World world, Action action, SingleEntity on) throws Exception {
+    private static boolean isInDepth(WorldGrid grid, SingleEntity ent1, SingleEntity ent2, int depth) {
         //TODO: Not implemented
+        return true;
+    }
+    private static void handleAll(World world, Action action) throws Exception {
+        Entity sourceEntity = Utils.findEntityByName(world, action.getBetween().getSourceEntity());
+
+        for (SingleEntity entity : sourceEntity.getSingleEntities())
+            handleSingle(world, action, entity);
+    }
+    private static void handleSingle(World world, Action action, SingleEntity on) throws Exception {
+        Entity targetEntity = Utils.findEntityByName(world, action.getBetween().getTargetEntity());
+        int depth = Integer.parseInt(ExpressionParser.evaluateExpression(world, action, action.getDepthExpression(), on));
+
+        for (SingleEntity singleTargetEntity : targetEntity.getSingleEntities())
+            if (isInDepth(world.getGrid(), on, singleTargetEntity, depth))
+                for (Action actToPerform : action.getActions().getActions())
+                    ActionsPerformer.fireAction(world, actToPerform, on);
+    }
+    public static void handle(World world, Action action, SingleEntity on) throws Exception {
+        if (Objects.isNull(on))
+            handleAll(world, action);
+
+        else
+            handleSingle(world, action, on);
     }
 }
