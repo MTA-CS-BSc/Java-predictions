85b6ddce450c2343cb459f5bb853c76ef0abc746
Maya Raskin <mayaraskin14@gmail.com>
2023-08-29 13:44:35 +0300

    Changed ResponseDTO

--- src/dtos/ResponseDTO.java
+++ src/dtos/ResponseDTO.java
@@ -1,23 +1,30 @@
 package dtos;
 
+import com.google.gson.Gson;
+
 public class ResponseDTO {
-    protected String message;
     protected int status;
     protected ErrorDescriptionDTO errorDescription;
-    public ResponseDTO(String _message, int _status) {
-        message = _message;
+    protected String data;
+    public ResponseDTO(int _status) {
         status = _status;
+        errorDescription = null;
+        data = null;
+    }
+
+    public ResponseDTO(int _status, Object _data) {
+        status = _status;
+        data = new Gson().toJson(_data);
         errorDescription = null;
     }
 
-    public ResponseDTO(String _message, int _status, String _errorCause, String _exceptionType) {
-        message = _message;
+    public ResponseDTO(int _status, Object _data, String _cause) {
         status = _status;
-        errorDescription = new ErrorDescriptionDTO(_errorCause, _exceptionType);
+        data = new Gson().toJson(_data);
+        errorDescription = new ErrorDescriptionDTO(_cause);
     }
 
-    public String getMessage() { return message; }
     public int getStatus() { return status; }
-
     public ErrorDescriptionDTO getErrorDescription() { return errorDescription; }
+    public String getData() { return data; }
 }
