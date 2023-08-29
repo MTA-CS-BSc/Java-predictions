e56e8debe83b213c06116e39fa0157005a4975ea
Maya Raskin <mayaraskin14@gmail.com>
2023-08-29 22:48:50 +0300

    Reformatted Entity

--- src/engine/prototypes/implemented/Entity.java
+++ src/engine/prototypes/implemented/Entity.java
@@ -11,7 +11,22 @@ public class Entity implements Serializable {
     protected int population;
     protected List<SingleEntity> singleEntities;
     protected Properties initialProperties;
+    public Entity(PRDEntity entity) {
+        name = entity.getName();
+        population = 0;
+        singleEntities = new ArrayList<>();
+        initialProperties = new Properties(entity.getPRDProperties().getPRDProperty());
+    }
+    public Entity(Entity other) {
+        name = other.getName();
+        population = other.getPopulation();
+        singleEntities = new ArrayList<>();
+        initialProperties = new Properties(other.getInitialProperties());
 
+        other.getSingleEntities().forEach(singleEntity -> {
+            singleEntities.add(new SingleEntity(singleEntity));
+        });
+    }
     public Properties getInitialProperties() {
         return initialProperties;
     }
@@ -27,29 +42,14 @@ public class Entity implements Serializable {
     public List<SingleEntity> getSingleEntities() {
         return singleEntities;
     }
-    public Entity(PRDEntity entity) {
-        name = entity.getName();
-        population = 0;
-        singleEntities = new ArrayList<>();
-        initialProperties = new Properties(entity.getPRDProperties().getPRDProperty());
+    public void setSingleEntities(List<SingleEntity> list) { singleEntities = list; }
+    public void initPopulation(int _population) {
+        //TODO: Add usage
+        setPopulation(_population);
 
         for (int i = 0; i < population; i++)
-            singleEntities.add(new SingleEntity(entity.getPRDProperties().getPRDProperty()));
+            singleEntities.add(new SingleEntity(getInitialProperties()));
     }
-
-    public Entity(Entity other) {
-        name = other.getName();
-        population = other.getPopulation();
-        singleEntities = new ArrayList<>();
-        initialProperties = new Properties(other.getInitialProperties());
-
-        other.getSingleEntities().forEach(singleEntity -> {
-           singleEntities.add(new SingleEntity(singleEntity));
-        });
-    }
-
-    public void setSingleEntities(List<SingleEntity> list) { singleEntities = list; }
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
