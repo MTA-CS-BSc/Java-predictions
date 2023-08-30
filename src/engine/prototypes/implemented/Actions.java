a97ae819d794bd26dcffa5611a75c6f6bd234638
Maya Raskin <mayaraskin14@gmail.com>
2023-08-30 21:12:15 +0300

    Removed redundant code

--- src/engine/prototypes/implemented/Actions.java
+++ src/engine/prototypes/implemented/Actions.java
@@ -1,7 +1,7 @@
 package engine.prototypes.implemented;
 
-import engine.consts.ActionTypes;
-import engine.prototypes.implemented.actions.*;
+import engine.prototypes.implemented.actions.Action;
+import engine.prototypes.implemented.actions.ActionCreator;
 import engine.prototypes.jaxb.PRDAction;
 
 import java.io.Serializable;
@@ -13,33 +13,11 @@ public class Actions implements Serializable {
     protected List<Action> actions = new ArrayList<>();
     public Actions(List<PRDAction> _actions) {
         _actions.forEach(action -> {
-            Action created = createAction(action);
+            Action created = ActionCreator.createAction(action);
 
             if (!Objects.isNull(created))
-                actions.add(createAction(action));
+                actions.add(created);
         });
-    }
-    private Action createAction(PRDAction action) {
-        switch (action.getType()) {
-            case ActionTypes.INCREASE:
-                return new IncreaseAction(action);
-            case ActionTypes.DECREASE:
-                return new DecreaseAction(action);
-            case ActionTypes.CALCULATION:
-                return new CalculationAction(action);
-            case ActionTypes.SET:
-                return new SetAction(action);
-            case ActionTypes.KILL:
-                return new KillAction(action);
-            case ActionTypes.REPLACE:
-                return new ReplaceAction(action);
-            case ActionTypes.CONDITION:
-                return new ConditionAction(action);
-            case ActionTypes.PROXIMITY:
-                return new ProximityAction(action);
-        }
-
-        return null;
     }
     public List<Action> getActions() { return actions; }
 }
