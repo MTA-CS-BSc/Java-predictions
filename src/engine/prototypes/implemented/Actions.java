5ca5360e4e3ac29946c62c76997476c782000266
Maya Raskin <mayaraskin14@gmail.com>
2023-08-30 20:54:00 +0300

    Added custom action creation on Actions

--- src/engine/prototypes/implemented/Actions.java
+++ src/engine/prototypes/implemented/Actions.java
@@ -1,17 +1,45 @@
 package engine.prototypes.implemented;
 
+import engine.consts.ActionTypes;
+import engine.prototypes.implemented.actions.*;
 import engine.prototypes.jaxb.PRDAction;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Actions implements Serializable {
     protected List<Action> actions = new ArrayList<>();
-
     public Actions(List<PRDAction> _actions) {
-        _actions.forEach(action -> actions.add(new Action(action)));
-    }
+        _actions.forEach(action -> {
+            Action created = createAction(action);
 
+            if (!Objects.isNull(created))
+                actions.add(createAction(action));
+        });
+    }
+    private Action createAction(PRDAction action) {
+        switch (action.getType()) {
+            case ActionTypes.INCREASE:
+                return new IncreaseAction(action);
+            case ActionTypes.DECREASE:
+                return new DecreaseAction(action);
+            case ActionTypes.CALCULATION:
+                return new CalculationAction(action);
+            case ActionTypes.SET:
+                return new SetAction(action);
+            case ActionTypes.KILL:
+                return new KillAction(action);
+            case ActionTypes.REPLACE:
+                return new ReplaceAction(action);
+            case ActionTypes.CONDITION:
+                return new ConditionAction(action);
+            case ActionTypes.PROXIMITY:
+                return new ProximityAction(action);
+        }
+
+        return null;
+    }
     public List<Action> getActions() { return actions; }
 }
