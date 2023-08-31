a134bbbc0ca64fb90e91640ae861a3e0fe8f5670
Maya Raskin <mayaraskin14@gmail.com>
2023-09-01 01:52:00 +0300

    Implemented ElapsedTimer for SingleSimulation

---
+++ src/engine/prototypes/implemented/ElapsedTimer.java
@@ -0,0 +1,41 @@
+package engine.prototypes.implemented;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ElapsedTimer {
+    private Timer timer;
+    private long startTime;
+    private long elapsedTime;
+    private boolean isRunning;
+
+    public ElapsedTimer() {
+        timer = new Timer();
+        isRunning = false;
+    }
+    public void startOrResume() {
+        if (!isRunning) {
+            isRunning = true;
+            startTime = System.currentTimeMillis();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    elapsedTime = System.currentTimeMillis() - startTime;
+                    System.out.println("Elapsed Time: " + elapsedTime + " milliseconds");
+                }
+            }, 0, 1000); // Update every 1000 milliseconds (1 second)
+        }
+    }
+
+    public void pause() {
+        if (isRunning) {
+            timer.cancel();
+            elapsedTime = System.currentTimeMillis() - startTime;
+            isRunning = false;
+        }
+    }
+
+    public long getElapsedTime() {
+        return elapsedTime;
+    }
+}
