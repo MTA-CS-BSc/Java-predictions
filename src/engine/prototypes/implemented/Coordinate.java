8c9c7ebfb7685c3575b88746a1c275400dc417ac
Maya Raskin <mayaraskin14@gmail.com>
2023-08-31 22:15:58 +0300

    Added equals override to Coordinate

--- src/engine/prototypes/implemented/Coordinate.java
+++ src/engine/prototypes/implemented/Coordinate.java
@@ -1,5 +1,7 @@
 package engine.prototypes.implemented;
 
+import java.util.Objects;
+
 public class Coordinate {
     protected int x;
     protected int y;
@@ -16,4 +18,12 @@ public class Coordinate {
 
     public void setX(int value) { x = value; }
     public void setY(int value) { y = value; }
+
+    @Override
+    public boolean equals(Object other) {
+        if (Objects.isNull(other) || other.getClass() != Coordinate.class)
+            return false;
+
+        return x == ((Coordinate)other).getX() && y == ((Coordinate)other).getY();
+    }
 }
