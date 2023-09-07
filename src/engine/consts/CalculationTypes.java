a08578fbcdfbd12d3f9c12ee9e87e9f220660548
Maya Raskin <mayaraskin14@gmail.com>
2023-09-07 23:28:56 +0300

    Added CalculationTypes & added type in CalculationAction

---
+++ src/engine/consts/CalculationTypes.java
@@ -0,0 +1,6 @@
+package engine.consts;
+
+public abstract class CalculationTypes {
+    public static final String MULTIPLY = "multiply";
+    public static final String DIVIDE = "divide";
+}
--- src/engine/prototypes/implemented/actions/CalculationAction.java
+++ src/engine/prototypes/implemented/actions/CalculationAction.java
@@ -1,5 +1,6 @@
 package engine.prototypes.implemented.actions;
 
+import engine.consts.CalculationTypes;
 import engine.prototypes.implemented.Divide;
 import engine.prototypes.implemented.Multiply;
 import engine.prototypes.jaxb.PRDAction;
@@ -10,16 +11,21 @@ public class CalculationAction extends Action {
     protected Multiply multiply;
     protected Divide divide;
     protected String resultPropertyName;
+    protected String operationType;
     public CalculationAction(PRDAction action) {
         super(action);
 
         resultPropertyName = action.getResultProp();
 
-        if (!Objects.isNull(action.getPRDMultiply()))
+        if (!Objects.isNull(action.getPRDMultiply())) {
             multiply = new Multiply(action.getPRDMultiply());
+            operationType = CalculationTypes.MULTIPLY;
+        }
 
-        if (!Objects.isNull(action.getPRDDivide()))
+        if (!Objects.isNull(action.getPRDDivide())) {
             divide = new Divide(action.getPRDDivide());
+            operationType = CalculationTypes.DIVIDE;
+        }
     }
     public Multiply getMultiply() {
         return multiply;
@@ -30,4 +36,5 @@ public class CalculationAction extends Action {
     public String getResultPropertyName() {
         return resultPropertyName;
     }
+    public String getOperationType() { return operationType; }
 }
