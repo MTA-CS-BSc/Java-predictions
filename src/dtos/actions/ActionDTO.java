33adf2a0809482433516151e5a7d933e58814c5c
Maya Raskin <mayaraskin14@gmail.com>
2023-09-07 23:00:50 +0300

    Added missing data member on ActionDTO

--- src/dtos/actions/ActionDTO.java
+++ src/dtos/actions/ActionDTO.java
@@ -1,13 +1,17 @@
 package dtos.actions;
 
+import java.util.Objects;
+
 public abstract class ActionDTO {
     protected String type;
     protected String entityName;
     protected SecondaryEntityDTO secondaryEntity;
+    protected boolean secondaryEntityExists;
     public ActionDTO(String type, String entityName, SecondaryEntityDTO secondaryEntity) {
         this.type = type;
         this.entityName = entityName;
         this.secondaryEntity = secondaryEntity;
+        this.secondaryEntityExists = !Objects.isNull(secondaryEntity);
     }
     public String getType() {
         return type;
@@ -17,5 +21,8 @@ public abstract class ActionDTO {
     }
     public SecondaryEntityDTO getSecondaryEntity() {
         return secondaryEntity;
+    }
+    public boolean isSecondaryEntityExists() {
+        return secondaryEntityExists;
     }
 }
