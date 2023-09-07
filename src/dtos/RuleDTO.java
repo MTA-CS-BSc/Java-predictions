76ecab3ceeb440f8a2f3efd9d5d0d90d987eed55
Maya Raskin <mayaraskin14@gmail.com>
2023-09-07 23:31:02 +0300

    Added actions list on RuleDTO

--- src/dtos/RuleDTO.java
+++ src/dtos/RuleDTO.java
@@ -1,48 +1,40 @@
 package dtos;
 
+import dtos.actions.ActionDTO;
+
+import java.util.List;
+import java.util.Objects;
+
 public class RuleDTO {
     protected String name;
     protected int ticks;
     protected double probability;
-    protected int actionsAmount;
-    public RuleDTO(String name, int ticks, double probability, int actionsAmount) {
+    protected List<ActionDTO> actions;
+    public RuleDTO(String name, int ticks, double probability, List<ActionDTO> actions) {
         this.name = name;
         this.ticks = ticks;
         this.probability = probability;
-        this.actionsAmount = actionsAmount;
+        this.actions = actions;
     }
+    public int getActionsAmount() { return !Objects.isNull(actions) ? actions.size() : 0; }
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getTicks() {
         return ticks;
     }
-
     public void setTicks(int ticks) {
         this.ticks = ticks;
     }
-
     public double getProbability() {
         return probability;
     }
-
     public void setProbability(double probability) {
         this.probability = probability;
     }
-
-    public int getActionsAmount() {
-        return actionsAmount;
-    }
-
-    public void setActionsAmount(int actionsAmount) {
-        this.actionsAmount = actionsAmount;
-    }
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -51,7 +43,6 @@ public class RuleDTO {
         sb.append("Name: ").append(getName()).append("\n");
         sb.append("Activation ticks: ").append(getTicks()).append("\n");
         sb.append("Activation probability: ").append(getProbability()).append("\n");
-        sb.append("Actions amount: ").append(getActionsAmount()).append("\n");
 
         return sb.toString();
     }
