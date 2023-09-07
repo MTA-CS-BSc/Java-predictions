384fa3e7dcb278e84ddf12e6bf97049a30cba9d0
Maya Raskin <mayaraskin14@gmail.com>
2023-09-07 23:00:20 +0300

    Reformatted

--- src/dtos/actions/SecondaryEntityDTO.java
+++ src/dtos/actions/SecondaryEntityDTO.java
@@ -2,10 +2,8 @@ package dtos.actions;
 
 public class SecondaryEntityDTO {
     protected String entityName;
-
     public SecondaryEntityDTO(String entityName) {
         this.entityName = entityName;
     }
-
     public String getEntityName() { return entityName; }
 }
