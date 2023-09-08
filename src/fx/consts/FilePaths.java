a998320b0b2bab1f83928e8d6fc200386c133647
Maya Raskin <mayaraskin14@gmail.com>
2023-09-08 22:13:38 +0300

    Fixed wrong file path

--- src/fx/consts/FilePaths.java
+++ src/fx/consts/FilePaths.java
@@ -1,5 +1,5 @@
 package fx.consts;
 
 public abstract class FilePaths {
-    public static final String WELCOME_SCREEN_FXML_PATH = "views/MainScreen.fxml";
+    public static final String WELCOME_SCREEN_FXML_PATH = "views/MainScreen/MainScreen.fxml";
 }
