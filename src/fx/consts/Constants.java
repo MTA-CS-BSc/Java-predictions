f176fd261933cdb032f1926a251f08b9f4336e1f
Maya Raskin <mayaraskin14@gmail.com>
2023-09-08 19:51:22 +0300

    Added constants classes

---
+++ src/fx/consts/Constants.java
@@ -0,0 +1,5 @@
+package fx.consts;
+
+public abstract class Constants {
+    public static final String APP_TITLE = "Predictions";
+}
---
+++ src/fx/consts/FilePaths.java
@@ -0,0 +1,5 @@
+package fx.consts;
+
+public abstract class FilePaths {
+    public static final String WELCOME_SCREEN_FXML_PATH = "views/WelcomeScreen.fxml";
+}
