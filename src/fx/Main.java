fd5d4f606635f5aff495e453e5acbe8f6fd07040
Maya Raskin <mayaraskin14@gmail.com>
2023-09-08 19:51:28 +0300

    Changed main accordingly

--- src/fx/Main.java
+++ src/fx/Main.java
@@ -1,5 +1,7 @@
 package fx;
 
+import fx.consts.Constants;
+import fx.consts.FilePaths;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,10 +13,10 @@ import java.util.Objects;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("views/WelcomeScreen.fxml")));
-
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(FilePaths.WELCOME_SCREEN_FXML_PATH)));
         Scene scene = new Scene(root);
-        stage.setTitle("Predictions");
+
+        stage.setTitle(Constants.APP_TITLE);
         stage.setScene(scene);
         stage.show();
     }
