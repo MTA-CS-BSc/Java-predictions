3e32e9eb07341c65bc58a45a47a7d9fd773a73d1
Maya Raskin <mayaraskin14@gmail.com>
2023-09-10 00:47:06 +0300

    Added env to WorldState

--- src/engine/prototypes/implemented/WorldState.java
+++ src/engine/prototypes/implemented/WorldState.java
@@ -6,13 +6,17 @@ import java.util.Map;
 
 public class WorldState implements Serializable {
     protected Map<String, Entity> entitiesMap;
+    protected Map<String, Property> environmentMap;
 
     public WorldState(World world) {
         entitiesMap = new HashMap<>();
-        world.getEntities().entitiesMap.forEach((key, entity) -> entitiesMap.put(key, new Entity(entity)));
+        environmentMap = new HashMap<>();
+        world.getEntities().getEntitiesMap().forEach((key, entity) -> entitiesMap.put(key, new Entity(entity)));
+        world.getEnvironment().getEnvVars().forEach((key, envVar) -> environmentMap.put(key, new Property(envVar)));
     }
 
     public Map<String, Entity> getEntitiesMap() {
         return entitiesMap;
     }
+    public Map<String, Property> getEnvironmentMap() { return environmentMap; }
 }
