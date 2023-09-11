dcaa4b83bf258c87e5c0f96e4b14225256fa4820
Maya Raskin <mayaraskin14@gmail.com>
2023-09-11 13:42:16 +0300

    Added DarkTheme.css & StyleSheetsPaths dict

---
+++ src/fx/views/StyleSheetsPaths.java
@@ -0,0 +1,7 @@
+package fx.views;
+
+public abstract class StyleSheetsPaths {
+    public static final String DEFAULT_THEME_CSS = "./views/AppBar/AppBarDefaultTheme.css";
+    public static final String DARK_THEME_CSS = "./views/AppBar/AppBarDarkTheme.css";
+    public static final String WINDOWS_7_THEME_CSS = "./views/AppBar/AppBarWin7Theme.css";
+}
