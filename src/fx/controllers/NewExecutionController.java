a59f794e7ed05bc51b4b6dbdb0258b58496a34ac
Maya Raskin <mayaraskin14@gmail.com>
2023-09-12 02:36:39 +0300

    implemented initialize entities table

--- src/fx/controllers/NewExecutionController.java
+++ src/fx/controllers/NewExecutionController.java
@@ -1,29 +1,53 @@
 package fx.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import dtos.EntityDTO;
+import fx.modules.SingletonEngineAPI;
+import helpers.SingletonObjectMapper;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
 
-import javax.swing.text.TableView;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class NewExecutionController implements Initializable {
     @FXML
-    private GridPane newExecutionGridPane;
+    private VBox container;
 
     @FXML
-    private TableView populationTable;
+    private TableView<EntityDTO> populationTable;
+
+    @FXML
+    private TableColumn<EntityDTO, String> entityNameColumn;
+
+    @FXML
+    private TableColumn<EntityDTO, Integer> populationColumn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        initializeEntitiesTable();
+        entityNameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
+        populationColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getPopulation()).asObject());
     }
 
-    private void initializeEntitiesTable() {
+    protected void initializeEntitiesTable(String uuid) throws JsonProcessingException {
+        //TODO: Add UI exception
+        if (uuid.isEmpty() || SingletonEngineAPI.api.getEntities(uuid).getStatus() != 200)
+            return;
+
+        List<EntityDTO> entities = SingletonObjectMapper.objectMapper.readValue(SingletonEngineAPI.api.getEntities(uuid).getData(),
+                    new TypeReference<List<EntityDTO>>() {});
+
+        populationTable.getItems().addAll(entities);
     }
 
-    public GridPane getGridPane() {
-        return newExecutionGridPane;
+    public VBox getContainer() {
+        return container;
     }
 }
