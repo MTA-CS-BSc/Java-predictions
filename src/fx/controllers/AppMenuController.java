5a60578e9dc26d4277f91b2ef3b654cca42138d7
Maya Raskin <mayaraskin14@gmail.com>
2023-09-11 16:08:58 +0300

    Added AppMenu fxml & controller

---
+++ src/fx/controllers/AppMenuController.java
@@ -0,0 +1,13 @@
+package fx.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AppMenuController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
