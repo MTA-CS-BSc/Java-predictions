f370cc76bef0c5649ccc528bfcce8888d39372a2
Maya Raskin <mayaraskin14@gmail.com>
2023-09-11 17:06:09 +0300

    Added createMainScene on Main to initialize screen & styles

--- src/fx/Main.java
+++ src/fx/Main.java
@@ -2,26 +2,30 @@ package fx;
 
 import fx.consts.Constants;
 import fx.consts.FilePaths;
-import fx.views.StyleSheetsPaths;
+import fx.consts.StyleSheetsPaths;
+import fx.controllers.DetailsScreenController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Objects;
-
 public class Main extends Application {
-    private Scene createMainScene(Parent root) {
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(String.valueOf(getClass().getResource(StyleSheetsPaths.DEFAULT_THEME_CSS)));
-        return scene;
+    private Scene createMainScene() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(FilePaths.DETAILS_SCREEN_FXML_PATH));
+        DetailsScreenController detailsScreenController = loader.getController();
+        Scene mainScene = new Scene(loader.load());
+
+        detailsScreenController.getAllScreensUpperBarController()
+                .getAppMenuController().addScene(mainScene);
+        detailsScreenController.getAllScreensUpperBarController()
+                .getAppMenuController().setTheme(mainScene, StyleSheetsPaths.DEFAULT_THEME_CSS);
+
+        return mainScene;
     }
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(FilePaths.DETAILS_SCREEN_FXML_PATH)));
         stage.setTitle(Constants.APP_TITLE);
-        stage.setScene(createMainScene(root));
+        stage.setScene(createMainScene());
         stage.show();
 
     }
