6c4f57582611cf8b56b00f54d6f9ac6819689c36
Maya Raskin <mayaraskin14@gmail.com>
2023-09-11 17:05:25 +0300

    Moved module

---
+++ src/fx/consts/StyleSheetsPaths.java
@@ -0,0 +1,7 @@
+package fx.consts;
+
+public abstract class StyleSheetsPaths {
+    public static final String DEFAULT_THEME_CSS = "../views/HeaderComponent/HeaderComponentDefaultTheme.css";
+    public static final String DARK_THEME_CSS = "../views/HeaderComponent/HeaderComponentDarkTheme.css";
+    public static final String WINDOWS_7_THEME_CSS = "../views/HeaderComponent/HeaderComponentWin7Theme.css";
+}
--- src/fx/views/StyleSheetsPaths.java
+++
@@ -1,7 +0,0 @@
-package fx.views;
-
-public abstract class StyleSheetsPaths {
-    public static final String DEFAULT_THEME_CSS = "./views/HeaderComponent/HeaderComponentDefaultTheme.css";
-    public static final String DARK_THEME_CSS = "./views/HeaderComponent/HeaderComponentDarkTheme.css";
-    public static final String WINDOWS_7_THEME_CSS = "./views/HeaderComponent/HeaderComponentWin7Theme.css";
-}
