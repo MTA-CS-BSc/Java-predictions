0367a1b14d9a1fdd8e570c2e44d0a8ab032bfe25
Maya Raskin <mayaraskin14@gmail.com>
2023-09-12 01:42:45 +0300

    refactored

--- src/fx/controllers/NewExecutionController.java
+++ src/fx/controllers/NewExecutionController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.GridPane;
 
+import javax.swing.text.TableView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,8 +12,16 @@ public class NewExecutionController implements Initializable {
     @FXML
     private GridPane newExecutionGridPane;
 
+    @FXML
+    private TableView populationTable;
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) { }
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeEntitiesTable();
+    }
+
+    private void initializeEntitiesTable() {
+    }
 
     public GridPane getGridPane() {
         return newExecutionGridPane;
