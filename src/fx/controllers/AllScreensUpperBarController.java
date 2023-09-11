8ed2a3da8522d46feb425ffaff9d81695aa842d7
Maya Raskin <mayaraskin14@gmail.com>
2023-09-11 16:09:12 +0300

    Added AllScreensUpperBar fxml & controller

---
+++ src/fx/controllers/AllScreensUpperBarController.java
@@ -0,0 +1,26 @@
+package fx.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AllScreensUpperBarController implements Initializable {
+    @FXML
+    private AppMenuController appMenuController;
+
+    @FXML
+    private HeaderComponentController headerComponentController;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) { }
+
+    public AppMenuController getAppMenuController() {
+        return appMenuController;
+    }
+
+    public HeaderComponentController getAppBarController() {
+        return headerComponentController;
+    }
+}
