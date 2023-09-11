75db684a8cf4c154a5720c76eb0c35aec5e49331
Maya Raskin <mayaraskin14@gmail.com>
2023-09-12 02:29:45 +0300

    Added population

--- src/fx/models/DetailsScreen/EntityModel.java
+++ src/fx/models/DetailsScreen/EntityModel.java
@@ -4,11 +4,19 @@ import java.util.List;
 
 public class EntityModel extends TreeItemModel {
     private final List<EntityPropertyModel> properties;
+    private int population;
+
     public EntityModel(String name, List<EntityPropertyModel> properties) {
         super(name);
         this.properties = properties;
+        this.population = 0;
     }
     public List<EntityPropertyModel> getProperties() {
         return properties;
+    }
+    public void setPopulation(int value) { population = value; }
+
+    public int getPopulation() {
+        return population;
     }
 }
