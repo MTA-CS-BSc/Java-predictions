ea06d19aefbfb5571d5f761a5c14f991e4a8fc39
Maya Raskin <mayaraskin14@gmail.com>
2023-09-11 17:05:17 +0300

    Added ThemeNames constants module

---
+++ src/fx/consts/ThemeNames.java
@@ -0,0 +1,7 @@
+package fx.consts;
+
+public abstract class ThemeNames {
+    public static final String DEFAULT = "Default";
+    public static final String DARK = "Dark";
+    public static final String WIN7 = "Windows 7";
+}
