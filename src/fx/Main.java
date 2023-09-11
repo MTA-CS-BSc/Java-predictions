3e5fb36ba1cef94eb1dfac90ad139cedcba766ee
Maya Raskin <mayaraskin14@gmail.com>
2023-09-11 13:43:04 +0300

    Added createMainScene to Main

--- src/fx/Main.java
+++ src/fx/Main.java
@@ -2,6 +2,7 @@ package fx;
 
 import fx.consts.Constants;
 import fx.consts.FilePaths;
+import fx.views.StyleSheetsPaths;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,13 +12,17 @@ import javafx.stage.Stage;
 import java.util.Objects;
 
 public class Main extends Application {
+    private Scene createMainScene(Parent root) {
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(String.valueOf(getClass().getResource(StyleSheetsPaths.DEFAULT_THEME_CSS)));
+        return scene;
+    }
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(FilePaths.DETAILS_SCREEN_FXML_PATH)));
-        Scene scene = new Scene(root);
-
         stage.setTitle(Constants.APP_TITLE);
-        stage.setScene(scene);
+        stage.setScene(createMainScene(root));
         stage.show();
+
     }
 }
