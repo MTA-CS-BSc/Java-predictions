20c72500dfda39aee7168271716c667a0163614d
Maya Raskin <mayaraskin14@gmail.com>
2023-09-12 14:12:12 +0300

    Added Alerts abstract class with showAlert function

---
+++ src/fx/modules/Alerts.java
@@ -0,0 +1,16 @@
+package fx.modules;
+
+import javafx.scene.control.Alert;
+import javafx.scene.layout.Region;
+
+public abstract class Alerts {
+    public static void showAlert(String title,  String header, String message, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setResizable(true);
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.setHeaderText(header);
+        alert.showAndWait();
+    }
+}
