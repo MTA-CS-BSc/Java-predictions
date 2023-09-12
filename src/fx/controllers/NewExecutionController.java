c22399a929b73aeebdbb6f2cd9b5ff8198144a49
Maya Raskin <mayaraskin14@gmail.com>
2023-09-12 14:12:37 +0300

    Added set to population edit commit

--- src/fx/controllers/NewExecutionController.java
+++ src/fx/controllers/NewExecutionController.java
@@ -1,17 +1,20 @@
 package fx.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import dtos.EntityDTO;
+import fx.modules.Alerts;
 import fx.modules.SingletonEngineAPI;
 import helpers.modules.SingletonObjectMapper;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.VBox;
+import javafx.util.converter.IntegerStringConverter;
 
 import java.net.URL;
 import java.util.List;
@@ -33,10 +36,29 @@ public class NewExecutionController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         entityNameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
+        populationColumn.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         populationColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getPopulation()).asObject());
     }
 
-    protected void initializeEntitiesTable(String uuid) throws JsonProcessingException {
+    private void addPopulationEditCommit(String uuid) {
+        populationColumn.setOnEditCommit(event -> {
+            EntityDTO editedEntity = event.getRowValue();
+
+            //TODO: Add ui exception of exceeds maximum space
+            if (event.getNewValue() < 0) {
+                Alerts.showAlert("Validation failed", "Population is invalid",
+                        "Population must be non-negative!", Alert.AlertType.ERROR);
+
+                editedEntity.setPopulation(event.getOldValue());
+                populationTable.refresh();
+                return;
+            }
+
+            SingletonEngineAPI.api.setEntityInitialPopulation(uuid, editedEntity.getName(), editedEntity.getPopulation());
+        });
+    }
+
+    protected void initializeEntitiesTable(String uuid) throws Exception {
         //TODO: Add UI exception
         if (uuid.isEmpty() || SingletonEngineAPI.api.getEntities(uuid).getStatus() != 200)
             return;
@@ -45,6 +67,7 @@ public class NewExecutionController implements Initializable {
                     new TypeReference<List<EntityDTO>>() {});
 
         populationTable.getItems().addAll(entities);
+        addPopulationEditCommit(uuid);
     }
 
     public VBox getContainer() {
