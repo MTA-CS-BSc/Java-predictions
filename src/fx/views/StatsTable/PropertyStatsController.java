374f6ed78e6d01fd0e23bd7e792fe879836543d0
Maya Raskin <mayaraskin14@gmail.com>
2023-09-15 22:56:10 +0300

    Added some basic skeleton

--- src/fx/views/StatsTable/PropertyStatsController.java
+++ src/fx/views/StatsTable/PropertyStatsController.java
@@ -1,22 +1,32 @@
 package fx.views.StatsTable;
 
+import dtos.SingleSimulationDTO;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PropertyStatsController implements Initializable {
     @FXML
-    private StackPane container;
+    private VBox container;
+
+    private ObjectProperty<SingleSimulationDTO> selectedSimulation;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        selectedSimulation = new SimpleObjectProperty<>();
         container.setVisible(false);
     }
 
     public void toggleVisibility() {
         container.setVisible(!container.isVisible());
+    }
+
+    public void setSelectedSimulation(SingleSimulationDTO simulation) {
+        selectedSimulation.setValue(simulation);
     }
 }
