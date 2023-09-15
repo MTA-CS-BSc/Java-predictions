002e01a2de772683f3d4084f19c8848796b3ac37
Maya Raskin <mayaraskin14@gmail.com>
2023-09-16 02:39:05 +0300

    added icons to FilePaths

--- src/fx/consts/FilePaths.java
+++ src/fx/consts/FilePaths.java
@@ -6,4 +6,6 @@ public abstract class FilePaths {
     public static final String RESUME_SIMULATION_ICON_PATH = "../../views/Resources/play-button.png";
     public static final String STOP_SIMULATION_ICON_PATH = "../../views/Resources/stop-button.png";
     public static final String RESTART_SIMULATION_ICON_PATH = "../../views/Resources/restart-button.png";
+    public static final String PAST_TRAVEL_ICON_PATH = "../../views/Resources/past-button.png";
+    public static final String FUTURE_TRAVEL_ICON_PATH = "../../views/Resources/future-button.png";
 }
