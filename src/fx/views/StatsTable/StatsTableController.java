d827454b701ed771c368f4cfe33f62418bb8af35
Maya Raskin <mayaraskin14@gmail.com>
2023-09-15 19:43:04 +0300

    Moved from StatsTable

--- src/fx/views/StatsTable/StatsTableController.java
+++ src/fx/views/StatsTable/StatsTableController.java
@@ -1,82 +1,41 @@
 package fx.views.StatsTable;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import dtos.SingleSimulationDTO;
-import fx.modules.SingletonEngineAPI;
-import helpers.modules.SingletonObjectMapper;
-import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.control.ComboBox;
 
 import java.net.URL;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class StatsTableController implements Initializable {
-
-    @FXML
-    private LineChart<Integer, Integer> entitiesAmountChart;
-
-    @FXML
-    private NumberAxis ticksAxis;
-
-    @FXML
-    private NumberAxis amountAxis;
-
     private ObjectProperty<SingleSimulationDTO> selectedSimulation;
+
+    @FXML
+    private ComboBox<String> filterByComboBox;
+
+    @FXML
+    private EntitiesAmountChartController entitiesAmountChartController;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         selectedSimulation = new SimpleObjectProperty<>();
 
+        filterByComboBox.getSelectionModel().selectedItemProperty()
+                        .addListener((observableValue, s, t1) -> {
+                            showStats();
+                        });
+
         selectedSimulation.addListener((observableValue, singleSimulationDTO, t1) -> {
-            if (Objects.isNull(t1))
-                entitiesAmountChart.getData().clear();
-
-            else if (!Objects.isNull(singleSimulationDTO) &&
-                        singleSimulationDTO.getUuid().equals(t1.getUuid())) {
-                // Skip
-            }
-
-            else {
-                try {
-                    ticksAxis.setUpperBound(t1.getTicks());
-                    fillChart();
-                } catch (Exception ignored) { }
-            }
-
+            entitiesAmountChartController.setSelectedSimulation(t1);
         });
     }
 
-    private void fillChart() throws JsonProcessingException {
-        Map<String, List<Integer>> entitiesAmountsPerTick = SingletonObjectMapper.objectMapper.readValue(
-                SingletonEngineAPI.api.getEntitiesAmountsPerTick(selectedSimulation.getValue().getUuid()).getData(),
-                new TypeReference<Map<String, List<Integer>>>() {}
-        );
+    public void showStats() {
 
-        Platform.runLater(() -> entitiesAmountChart.getData().clear());
-
-        entitiesAmountsPerTick.forEach((key, amountsListPerTick) -> {
-            XYChart.Series<Integer, Integer> series =  new XYChart.Series<>();
-            series.setName(key);
-
-            for (int i = 0; i < amountsListPerTick.size(); i++)
-                series.getData().add(new XYChart.Data<>(i, amountsListPerTick.get(i)));
-
-            Platform.runLater(() -> {
-                entitiesAmountChart.getData().add(series);
-            });
-        });
     }
-
     public void setSelectedSimulation(SingleSimulationDTO simulation) {
         selectedSimulation.setValue(simulation);
     }
