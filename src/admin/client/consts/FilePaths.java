804377050e7f7a6d7b9d0fd62a2e4d4a751f86bc
Maya Raskin <mayaraskin14@gmail.com>
2023-10-01 00:02:16 +0300

    changed path

--- src/admin/client/consts/FilePaths.java
+++ src/admin/client/consts/FilePaths.java
@@ -1,5 +1,5 @@
 package admin.client.consts;
 
 public class FilePaths {
-    public static final String ADMIN_ORCHESTRATOR_FXML = "orchestrator/Orchestrator.fxml";
+    public static final String ADMIN_ORCHESTRATOR_FXML = "views/orchestrator/Orchestrator.fxml";
 }
