06335d0d88a63fe24d3ea0edf3da0315fd85f6e4
Maya Raskin <mayaraskin14@gmail.com>
2023-09-30 15:26:20 +0300

    Removed termination validators

--- src/engine/validators/PRDWorldValidators.java
+++ src/engine/validators/PRDWorldValidators.java
@@ -2,12 +2,10 @@ package engine.validators;
 
 import dtos.ResponseDTO;
 import engine.exceptions.GridSizeException;
-import engine.exceptions.ValueNotInRangeException;
 import engine.prototypes.jaxb.PRDWorld;
 import engine.validators.entities.PRDEntitiesValidators;
 import engine.validators.env.PRDEnvironmentValidators;
 import engine.validators.rules.PRDRulesValidators;
-import engine.validators.termination.PRDTerminationValidators;
 import helpers.Constants;
 
 public abstract class PRDWorldValidators {
@@ -15,9 +13,8 @@ public abstract class PRDWorldValidators {
         StringTrimmer.trimAllStrings(world);
 
         try {
-            if (validateEntities(world) && validateTermination(world)
-                    && validateEnvironment(world) && validateRules(world)
-                    && validateThreadpool(world) && validateGrid(world))
+            if (validateEntities(world) && validateEnvironment(world) &&
+                    validateRules(world) && validateGrid(world))
                 return new ResponseDTO(200, "XML validation passed");
 
             return new ResponseDTO(500, "XML validation failed", "Unknown");
@@ -30,23 +27,12 @@ public abstract class PRDWorldValidators {
         return PRDEntitiesValidators.validateEntities(world);
     }
 
-    private static boolean validateTermination(PRDWorld world) throws Exception {
-        return PRDTerminationValidators.validateStopConditionExists(world);
-    }
-
     private static boolean validateEnvironment(PRDWorld world) throws Exception {
         return PRDEnvironmentValidators.validateEnvironment(world);
     }
 
     private static boolean validateRules(PRDWorld world) throws Exception {
         return PRDRulesValidators.validateRules(world);
-    }
-
-    private static boolean validateThreadpool(PRDWorld world) throws Exception {
-        if (world.getPRDThreadCount() <= 0)
-            throw new ValueNotInRangeException("Threadpool count is less or equal to zero");
-
-        return true;
     }
 
     private static boolean validateGrid(PRDWorld world) throws Exception {
