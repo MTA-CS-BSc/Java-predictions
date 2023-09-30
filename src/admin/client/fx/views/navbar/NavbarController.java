6c34ba85979aae3cb1aa5f1d95de36b0848421f6
Maya Raskin <mayaraskin14@gmail.com>
2023-09-30 19:35:49 +0300

    Added Navbar fxml & controller

---
+++ src/admin/client/fx/views/navbar/NavbarController.java
@@ -0,0 +1,12 @@
+package admin.client.fx.views.navbar;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NavbarController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) { }
+}
