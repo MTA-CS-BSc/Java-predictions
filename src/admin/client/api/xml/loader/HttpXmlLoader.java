3f2f9a158fd7f0bb1e00bacd33e2e7a7fe3f09bf
Maya Raskin <mayaraskin14@gmail.com>
2023-10-01 21:01:55 +0300

    Implemented HttpXmlLoader.uploadXml

--- src/admin/client/api/xml/loader/HttpXmlLoader.java
+++ src/admin/client/api/xml/loader/HttpXmlLoader.java
@@ -1,18 +1,15 @@
 package admin.client.api.xml.loader;
 
 import admin.client.api.Configuration;
-import dtos.ResponseDTO;
-import helpers.modules.SingletonObjectMapper;
 import okhttp3.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Objects;
 
 public abstract class HttpXmlLoader {
     private static final String XML_LOAD_URL = Configuration.SERVER_URL + "/xml/load";
 
-    public static ResponseDTO uploadXml(File file) throws IOException {
+    public static Response uploadXml(File file) throws IOException {
         RequestBody requestBody = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
                 .addFormDataPart("fileField",file.getName(), RequestBody.create(file, MediaType.parse("application/octet-stream")))
@@ -23,11 +20,6 @@ public abstract class HttpXmlLoader {
                 .post(requestBody)
                 .build();
 
-        Response httpResponse = Configuration.HTTP_CLIENT.newCall(request).execute();
-
-        if (!httpResponse.isSuccessful() || Objects.isNull(httpResponse.body()) || httpResponse.body().string().isEmpty())
-            return new ResponseDTO(500, false, "Unknown");
-
-        return SingletonObjectMapper.objectMapper.readValue(httpResponse.body().string(), ResponseDTO.class);
+        return Configuration.HTTP_CLIENT.newCall(request).execute();
     }
 }
