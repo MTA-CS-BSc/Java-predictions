6f6f0ab2548eef2b2b60d5d7441c41f7a4f95872
Maya Raskin <mayaraskin14@gmail.com>
2023-10-01 21:01:32 +0300

    added status constants

--- src/helpers/Constants.java
+++ src/helpers/Constants.java
@@ -14,5 +14,7 @@ public class Constants {
     public final static int MAX_RANDOM_STRING_LENGTH = 50;
     public final static Duration ANIMATION_DURATION = new javafx.util.Duration(1000);
     public final static int API_RESPONSE_OK = 200;
+    public final static int API_RESPONSE_SERVER_ERROR = 500;
+    public final static int API_RESPONSE_BAD_REQUEST = 400;
     public final static int API_REFETCH_INTERVAL_MILLIS = 1500;
 }
