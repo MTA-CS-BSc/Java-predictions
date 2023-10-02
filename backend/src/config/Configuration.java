ab29c277aab5759a6d2b46cca1996d988b392f37
Maya Raskin <mayaraskin14@gmail.com>
2023-10-03 00:39:14 +0300

    Added json return

--- backend/src/config/Configuration.java
+++ backend/src/config/Configuration.java
@@ -3,5 +3,6 @@ package config;
 import api.EngineAPI;
 
 public abstract class Configuration {
+    public static final String JSON_CONTENT_TYPE = "application/json";
     public static final EngineAPI api = new EngineAPI();
 }
--- backend/src/servlets/XmlLoadServlet.java
+++ backend/src/servlets/XmlLoadServlet.java
@@ -2,14 +2,14 @@ package servlets;
 
 import config.Configuration;
 import jakarta.servlet.annotation.MultipartConfig;
-import other.ResponseDTO;
-import modules.Constants;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
+import json.JsonParser;
+import modules.Constants;
+import other.ResponseDTO;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,21 +19,31 @@ import java.util.Objects;
 @WebServlet(name = "XmlLoadServlet", urlPatterns = "/xml/load")
 public class XmlLoadServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Part filePart = request.getPart("fileField");
-        InputStream fileContent = filePart.getInputStream();
-        response.setContentType("text/plain");
-        ResponseDTO responseDTO = null;
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType(Configuration.JSON_CONTENT_TYPE);
 
         try {
-            responseDTO = Configuration.api.loadXml(fileContent);
-            response.setStatus(Constants.API_RESPONSE_OK);
-            response.getWriter().write("OK");
-        } catch (Exception e) {
-            response.setStatus(Constants.API_RESPONSE_SERVER_ERROR);
+            Part filePart = request.getPart("fileField");
+            InputStream fileContent = filePart.getInputStream();
+            ResponseDTO responseDTO = Configuration.api.loadXml(fileContent);
 
-            if (!Objects.isNull(responseDTO) && !Objects.isNull(responseDTO.getErrorDescription()))
-                response.getWriter().write(responseDTO.getErrorDescription().getCause());
+            if (responseDTO.getStatus() == Constants.API_RESPONSE_OK) {
+                response.setStatus(Constants.API_RESPONSE_OK);
+                response.getWriter().write(JsonParser.toJson("data", "OK"));
+            }
+
+            else {
+                response.setStatus(Constants.API_RESPONSE_BAD_REQUEST);
+
+                if (!Objects.isNull(responseDTO.getErrorDescription()))
+                    response.getWriter().write(JsonParser.toJson("detail", responseDTO.getErrorDescription().getCause()));
+            }
+        }
+
+        catch (IOException e) {
+            response.setStatus(Constants.API_RESPONSE_SERVER_ERROR);
+        } catch (Exception se) {
+            response.setStatus(Constants.API_RESPONSE_BAD_REQUEST);
         }
     }
 }
