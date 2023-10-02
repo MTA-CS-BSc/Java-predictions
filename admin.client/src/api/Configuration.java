afe3380342071455683efc41ef282995236b602a
Maya Raskin <mayaraskin14@gmail.com>
2023-10-03 01:20:28 +0300

    Added json return

--- admin.client/src/api/Configuration.java
+++ admin.client/src/api/Configuration.java
@@ -3,6 +3,6 @@ package api;
 import okhttp3.OkHttpClient;
 
 public abstract class Configuration {
-    public final static String SERVER_URL = "http://localhost:8080";
+    public final static String SERVER_URL = "http://localhost:8080/backend_Web";
     public final static OkHttpClient HTTP_CLIENT = new OkHttpClient();
 }
