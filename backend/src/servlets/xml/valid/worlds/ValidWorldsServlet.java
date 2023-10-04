18dbe4538f3bf5f7e88e90e5c3cb8d64094af1b3
Maya Raskin <mayaraskin14@gmail.com>
2023-10-04 17:15:06 +0300

    Implemented ValidWorlds Servlet - get

---
+++ backend/src/servlets/xml/valid/worlds/ValidWorldsServlet.java
@@ -0,0 +1,34 @@
+package servlets.xml.valid.worlds;
+
+import api.Routes;
+import config.Configuration;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import json.JsonParser;
+import json.Keys;
+import modules.Constants;
+import other.ResponseDTO;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@WebServlet(Routes.XML_VALID_WORLDS)
+public class ValidWorldsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType(Constants.JSON_CONTENT_TYPE);
+
+        ResponseDTO responseDTO = Configuration.api.getAllValidWorldsNames();
+        resp.setStatus(responseDTO.getStatus());
+
+        if (!Objects.isNull(responseDTO.getErrorDescription()))
+            resp.getWriter().write(JsonParser.toJson(Keys.INVALID_RESPONSE_KEY, responseDTO.getErrorDescription().getCause()));
+
+        else
+            resp.getWriter().write(responseDTO.getData());
+
+    }
+}
