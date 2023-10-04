e3e0caccb851e80603e6cbea8ee36923cd3b65b0
Maya Raskin <mayaraskin14@gmail.com>
2023-10-04 18:51:01 +0300

    Started implementing WorldDetails

--- admin_client/src/fx/mgmt/controllers/MgmtController.java
+++ admin_client/src/fx/mgmt/controllers/MgmtController.java
@@ -2,6 +2,7 @@ package fx.mgmt.controllers;
 
 import consts.Animations;
 import fx.mgmt.threadpool.controllers.ThreadsAmountSetterController;
+import fx.mgmt.world.details.controllers.WorldDetailsController;
 import fx.xml.loader.controllers.XmlLoaderController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,6 +13,7 @@ import java.util.ResourceBundle;
 public class MgmtController implements Initializable {
     @FXML private XmlLoaderController xmlLoaderController;
     @FXML private ThreadsAmountSetterController threadsAmountSetterController;
+    @FXML private WorldDetailsController worldDetailsController;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
