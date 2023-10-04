451a0ff33a5df291e8c93d31c1d101f5e4f296a1
Maya Raskin <mayaraskin14@gmail.com>
2023-10-04 16:05:52 +0300

    added routes

--- helpers/src/api/Routes.java
+++ helpers/src/api/Routes.java
@@ -2,6 +2,9 @@ package api;
 
 public abstract class Routes {
     public static final String THREADPOOL = "/threadpool";
+    public static final String XML = "/xml";
     public static final String THREADPOOL_QUEUE = THREADPOOL + "/queue";
-    public static final String XML_LOAD = "/xml/load";
+    public static final String XML_LOAD = XML + "/load";
+    public static final String XML_VALID = XML + "/valid";
+    public static final String XML_VALID_WORLDS = XML_VALID + "/worlds";
 }
