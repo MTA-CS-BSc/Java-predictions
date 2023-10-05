a66e0f8b79046a81d3bd00ae27f8d76910d29dd4
Maya Raskin <mayaraskin14@gmail.com>
2023-10-05 15:27:08 +0300

    Implemented client get for past simulations

---
+++ admin_client/src/api/history/simulations/HttpPastSimulations.java
@@ -0,0 +1,21 @@
+package api.history.simulations;
+
+import api.Configuration;
+import api.Routes;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public abstract class HttpPastSimulations {
+    public static final String PAST_SIMULATIONS_URL = Configuration.SERVER_URL + Routes.SIMULATIONS_HISTORY;
+
+    public static Response getPastSimulations() throws IOException {
+        Request request = new Request.Builder()
+                .url(PAST_SIMULATIONS_URL)
+                .get()
+                .build();
+
+        return Configuration.HTTP_CLIENT.newCall(request).execute();
+    }
+}
