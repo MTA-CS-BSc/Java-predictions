620fc5f1af0171fc940d6dae66a80fda9a021879
Maya Raskin <mayaraskin14@gmail.com>
2023-10-06 20:33:43 +0300

    added setIsParentVisible on RequestsTableController

--- user_client/src/fx/components/requests/RequestsController.java
+++ user_client/src/fx/components/requests/RequestsController.java
@@ -1,5 +1,6 @@
 package fx.components.requests;
 
+import fx.components.requests.table.RequestsTableController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
@@ -9,11 +10,13 @@ import java.util.ResourceBundle;
 
 public class RequestsController implements Initializable {
     @FXML private VBox container;
+    @FXML private RequestsTableController requestsTableController;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        requestsTableController.setIsParentVisibleProperty(container.visibleProperty());
     }
+
     public VBox getContainer() {
         return container;
     }
