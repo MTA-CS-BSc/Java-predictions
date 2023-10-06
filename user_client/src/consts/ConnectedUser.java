41435fea40b6ea31fb654a9adac5f58a03878a2d
Maya Raskin <mayaraskin14@gmail.com>
2023-10-06 20:34:38 +0300

    Added ConnectedUser

---
+++ user_client/src/consts/ConnectedUser.java
@@ -0,0 +1,9 @@
+package consts;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public abstract class ConnectedUser {
+    public static StringProperty USERNAME_PROPERTY = new SimpleStringProperty();
+
+}
