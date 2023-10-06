299e2c91b237681785d28c7da76854109ea03d1f
Maya Raskin <mayaraskin14@gmail.com>
2023-10-06 20:33:12 +0300

    removed redundant code

--- admin_client/src/api/allocations/HttpAllocations.java
+++ admin_client/src/api/allocations/HttpAllocations.java
@@ -8,11 +8,8 @@ import json.Keys;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import other.TerminationDTO;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public abstract class HttpAllocations {
     public static final String ALLOCATIONS_URL = Configuration.SERVER_URL + Routes.ALLOCATIONS_HISTORY;
@@ -23,25 +20,6 @@ public abstract class HttpAllocations {
         Request request = new Request.Builder()
                 .url(ALLOCATIONS_URL)
                 .get()
-                .build();
-
-        return Configuration.HTTP_CLIENT.newCall(request).execute();
-    }
-
-    public static Response createAllocationRequest(String createdUser,
-                                                   String initialWorldName,
-                                                   int executionsAmount,
-                                                   TerminationDTO termination) throws IOException {
-        Map<String, Object> body = new HashMap<>();
-
-        body.put(Keys.CREATED_USER_KEY, createdUser);
-        body.put(Keys.INITIAL_WORLD_NAME_KEY, initialWorldName);
-        body.put(Keys.REQUESTED_EXECUTIONS_KEY, executionsAmount);
-        body.put(Keys.TERMINATION_KEY, JsonParser.toJson(termination));
-
-        Request request = new Request.Builder()
-                .url(ALLOCATIONS_URL)
-                .post(RequestBody.create(JsonParser.toJson(body), API.JSON_MEDIA_TYPE))
                 .build();
 
         return Configuration.HTTP_CLIENT.newCall(request).execute();
