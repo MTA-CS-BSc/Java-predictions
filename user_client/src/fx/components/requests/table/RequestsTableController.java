1cdb7eb85a8e606f30795d815729e1eb00b2fc05
Maya Raskin <mayaraskin14@gmail.com>
2023-10-06 20:34:29 +0300

    started implementing requests table

--- user_client/src/fx/components/requests/table/RequestsTableController.java
+++ user_client/src/fx/components/requests/table/RequestsTableController.java
@@ -1,26 +1,123 @@
 package fx.components.requests.table;
 
+import api.ApiConstants;
+import api.requests.HttpAllocations;
+import com.fasterxml.jackson.core.type.TypeReference;
+import consts.ConnectedUser;
+import javafx.application.Platform;
+import javafx.beans.property.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import json.JsonParser;
+import okhttp3.Response;
 import other.AllocationRequestDTO;
 import types.RequestState;
+import types.SimulationState;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class RequestsTableController implements Initializable {
-    @FXML private TableView<AllocationRequestDTO> requestsTableView;
+    @FXML private TableView<AllocationRequestDTO> allocationsTableView;
     @FXML private TableColumn<AllocationRequestDTO, String> requestUuidColumn;
     @FXML private TableColumn<AllocationRequestDTO, String> worldNameColumn;
     @FXML private TableColumn<AllocationRequestDTO, Integer> executionsAmountColumn;
     @FXML private TableColumn<AllocationRequestDTO, RequestState> stateColumn;
-    @FXML private TableColumn<AllocationRequestDTO, Integer> runningSimulationsColumn;
-    @FXML private TableColumn<AllocationRequestDTO, Integer> finishedSimulationsColumn;
+    @FXML private TableColumn<AllocationRequestDTO, Integer> runningSimulationsInRequestColumn;
+    @FXML private TableColumn<AllocationRequestDTO, Integer> finishedSimulationsInRequestColumn;
+    //TODO: Init field
+    @FXML private TableColumn<AllocationRequestDTO, Boolean> executeColumn;
+    private ObjectProperty<AllocationRequestDTO> selectedRequest;
+
+    private BooleanProperty isParentVisible;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //TODO: Change
+        ConnectedUser.USERNAME_PROPERTY.setValue("maya");
 
+        selectedRequest = new SimpleObjectProperty<>();
+        isParentVisible = new SimpleBooleanProperty();
+        initColumns();
+
+        Executors.newScheduledThreadPool(1)
+                .scheduleAtFixedRate(this::fetchAllocationRequests, 0,
+                        ApiConstants.API_REFETCH_INTERVAL_MILLIS, TimeUnit.MILLISECONDS);
+    }
+
+    private void initColumns() {
+        requestUuidColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getUuid()));
+        worldNameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getInitialWorldName()));
+        stateColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getState()));
+        executionsAmountColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getRequestedExecutions()).asObject());
+        runningSimulationsInRequestColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty((int)cellData.getValue().getRequestSimulations().stream().filter(element -> element.getSimulationState() == SimulationState.RUNNING).count()).asObject());
+        finishedSimulationsInRequestColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty((int)cellData.getValue().getRequestSimulations().stream().filter(element -> element.getSimulationState() == SimulationState.FINISHED).count()).asObject());
+    }
+
+    private void fetchAllocationRequests() {
+        if (isParentVisible.getValue()) {
+            try {
+                Response response = HttpAllocations.getAllocationsRequests(ConnectedUser.USERNAME_PROPERTY.getValue());
+
+                if (!response.isSuccessful()) {
+                    response.close();
+                    return;
+                }
+
+                if (!Objects.isNull(response.body())) {
+                    List<AllocationRequestDTO> requests = JsonParser.objectMapper.readValue(
+                            response.body().string(),
+                            new TypeReference<List<AllocationRequestDTO>>() {
+                            });
+
+                    Platform.runLater(() -> {
+                        allocationsTableView.getItems().clear();
+                        allocationsTableView.getItems().addAll(requests);
+                        allocationsTableView.refresh();
+
+                        if (!Objects.isNull(selectedRequest.getValue()))
+                            selectPreviouslySelected();
+                    });
+                }
+            } catch (Exception ignored) { }
+        }
+    }
+
+    private void selectPreviouslySelected() {
+        if (Objects.isNull(selectedRequest.getValue()))
+            return;
+
+        AllocationRequestDTO newlySelectedRequest = allocationsTableView.getItems()
+                .stream()
+                .filter(element -> element.getUuid().equals(selectedRequest.getValue().getUuid()))
+                .findFirst().orElse(null);
+
+        allocationsTableView.getSelectionModel().select(newlySelectedRequest);
+        setSelectedRequest(newlySelectedRequest);
+    }
+
+    public void setIsParentVisibleProperty(BooleanProperty property) {
+        isParentVisible = property;
+    }
+
+    public void setSelectedRequest(AllocationRequestDTO value) {
+        selectedRequest.setValue(value);
+    }
+
+    @FXML
+    private void handleRequestClicked(MouseEvent event) {
+        if (event.getClickCount() == 1) {
+            AllocationRequestDTO selected = allocationsTableView.getSelectionModel().getSelectedItem();
+
+            if (selected != null)
+                setSelectedRequest(selected);
+        }
     }
 }
