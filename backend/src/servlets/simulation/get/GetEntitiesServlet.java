e59cf14a38058e4ed12570679b595d3416f504b6
Maya Raskin <mayaraskin14@gmail.com>
2023-10-07 17:26:28 +0300

    commented some servlets

--- backend/src/servlets/simulation/get/GetEntitiesServlet.java
+++ backend/src/servlets/simulation/get/GetEntitiesServlet.java
@@ -1,43 +1,35 @@
 package servlets.simulation.get;
 
-import api.ApiConstants;
 import api.Routes;
-import config.Configuration;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import json.JsonParser;
-import json.Keys;
-import other.ResponseDTO;
-import types.TypesUtils;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.Objects;
 
 @WebServlet(Routes.SIMULATION_ENTITIES)
 public class GetEntitiesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType(ApiConstants.JSON_CONTENT_TYPE);
-        Map<String, Object> requestBody = JsonParser.getRequestBodyMap(req.getReader());
-        String simulationUuid = requestBody.get(Keys.UUID_KEY).toString();
-        boolean isInitial = JsonParser.objectMapper.readValue(
-                requestBody.get(Keys.IS_INITIAL_KEY).toString(),
-                Boolean.class
-        );
-
-        if (!TypesUtils.isNullOrEmpty(simulationUuid)) {
-            ResponseDTO responseDTO = Configuration.api.getEntities(simulationUuid, isInitial);
-            resp.setStatus(responseDTO.getStatus());
-
-            if (!Objects.isNull(responseDTO.getErrorDescription()))
-                resp.getWriter().write(JsonParser.toJson(Keys.INVALID_RESPONSE_KEY, responseDTO.getErrorDescription().getCause()));
-
-            else
-                resp.getWriter().write(responseDTO.getData());
-        }
+//        resp.setContentType(ApiConstants.JSON_CONTENT_TYPE);
+//        Map<String, Object> requestBody = JsonParser.getRequestBodyMap(req.getReader());
+//        String simulationUuid = requestBody.get(Keys.UUID_KEY).toString();
+//        boolean isInitial = JsonParser.objectMapper.readValue(
+//                requestBody.get(Keys.IS_INITIAL_KEY).toString(),
+//                Boolean.class
+//        );
+//
+//        if (!TypesUtils.isNullOrEmpty(simulationUuid)) {
+//            ResponseDTO responseDTO = Configuration.api.getEntities(simulationUuid, isInitial);
+//            resp.setStatus(responseDTO.getStatus());
+//
+//            if (!Objects.isNull(responseDTO.getErrorDescription()))
+//                resp.getWriter().write(JsonParser.toJson(Keys.INVALID_RESPONSE_KEY, responseDTO.getErrorDescription().getCause()));
+//
+//            else
+//                resp.getWriter().write(responseDTO.getData());
+//        }
     }
 }
