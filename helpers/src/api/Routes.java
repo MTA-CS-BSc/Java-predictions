c7033f66d18bbd546e5f91b853e7b449a528e149
Maya Raskin <mayaraskin14@gmail.com>
2023-10-07 17:48:50 +0300

    Added backend servlet for get creating

--- helpers/src/api/Routes.java
+++ helpers/src/api/Routes.java
@@ -22,10 +22,12 @@ public abstract class Routes {
     public static final String ENTITITY_COUNT_FOR_PROP = PROPERTY_STATS + "/entity/count";
     public static final String ENTITIES_AMOUNTS_PER_TICK = RESULTS + "/entities/amounts/pertick";
     public static final String SIMULATION = "/simulation";
+    public static final String CREATING_SIMULATION = SIMULATION + "/creating";
     public static final String SIMULATION_ENTITIES = SIMULATION + "/entities";
     public static final String SIMULATION_ENV_PROPS = SIMULATION + "/environment";
     public static final String RUN_SIMULATION = SIMULATION + "/run";
     public static final String CREATE_SIMULATION = SIMULATION + "/create";
+    public static final String ENQUEUE_SIMULATION = SIMULATION + "/enqueue";
     public static final String CLONE_SIMULATION = SIMULATION + "/clone";
     public static final String STOP_SIMULATION = SIMULATION + "/stop";
     public static final String RESUME_SIMULATION = SIMULATION + "/resume";
