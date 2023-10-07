6b88e875c0ba6069efce51bdd537851f4fff61c5
Maya Raskin <mayaraskin14@gmail.com>
2023-10-07 23:16:27 +0300

    Implemented Execution.fxml & ExecutionController

--- user_client/src/fx/components/execution/ExecutionController.java
+++ user_client/src/fx/components/execution/ExecutionController.java
@@ -1,13 +1,33 @@
 package fx.components.execution;
 
+import api.simulation.HttpSimulation;
+import api.simulation.set.HttpSimulationSetters;
+import consts.Alerts;
+import consts.Animations;
+import fx.components.header.navbar.NavbarController;
+import fx.components.population.table.PopulationTableController;
+import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.VBox;
+import modules.Restrictions;
+import okhttp3.Response;
+import other.PropertyDTO;
+import other.RangeDTO;
 import other.SingleSimulationDTO;
+import tray.notification.NotificationType;
+import tray.notification.TrayNotification;
+import types.SimulationState;
 
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class ExecutionController implements Initializable {
@@ -15,14 +35,27 @@ public class ExecutionController implements Initializable {
 
     private ObjectProperty<SingleSimulationDTO> creatingSimulation;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        creatingSimulation = new SimpleObjectProperty<>();
+    @FXML
+    private PopulationTableController populationTableController;
 
-        creatingSimulation.addListener((observableValue, singleSimulationDTO, t1) -> {
-            //TODO: Implement
-        });
-    }
+//    #region Environment Variables Table
+    @FXML
+    private TableView<PropertyDTO> envPropsTable;
+
+    @FXML
+    private TableColumn<PropertyDTO, String> propertyNameColumn;
+
+    @FXML
+    private TableColumn<PropertyDTO, String> propertyTypeColumn;
+
+    @FXML
+    private TableColumn<PropertyDTO, RangeDTO> propertyRangeColumn;
+
+    @FXML
+    private TableColumn<PropertyDTO, String> propertyValueColumn;
+    //#endregion
+
+    private NavbarController navbarController;
 
     public ObjectProperty<SingleSimulationDTO> creatingSimulationProperty() {
         return creatingSimulation;
@@ -32,7 +65,118 @@ public class ExecutionController implements Initializable {
         creatingSimulation.setValue(value);
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        creatingSimulation = new SimpleObjectProperty<>();
+        initEnvPropsTable();
+
+        creatingSimulation.addListener((observableValue, singleSimulationDTO, t1) -> {
+            populationTableController.setSelectedSimulation(t1);
+
+            if (Objects.isNull(t1))
+                envPropsTable.getItems().clear();
+
+            else {
+                if (t1.getSimulationState() == SimulationState.CREATED)
+                    addValueEditCommit();
+
+                envPropsTable.getItems().clear();
+                envPropsTable.getItems().addAll(t1.getWorld().getEnvironment());
+                envPropsTable.refresh();
+            }
+        });
+    }
+
+    public void setNavbarController(NavbarController controller) {
+        navbarController = controller;
+    }
+
+    private void initEnvPropsTable() {
+        propertyNameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
+        propertyTypeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getType()));
+        propertyRangeColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getRange()));
+        propertyValueColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        propertyValueColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getValue()));
+    }
+
+    private void addValueEditCommit() {
+        propertyValueColumn.setOnEditCommit(event -> {
+            PropertyDTO editedProperty = event.getRowValue();
+
+            try {
+                Response response = HttpSimulationSetters.setEnvironmentProperty(creatingSimulation.getValue().getUuid(),
+                        editedProperty.getName(), Integer.parseInt(event.getNewValue()));
+
+                if (!response.isSuccessful()) {
+                    if (!Objects.isNull(response.body()))
+                        Alerts.showAlert("Validation failed", response.body().string(), Alert.AlertType.ERROR);
+
+                    response.close();
+
+                    editedProperty.setValue(event.getOldValue());
+                    envPropsTable.refresh();
+                }
+
+                else
+                    editedProperty.setValue(event.getNewValue());
+            } catch (Exception ignored) { }
+        });
+    }
+
     public VBox getContainer() {
         return container;
+    }
+
+    public boolean isSimulationEmpty() {
+        return Objects.isNull(creatingSimulation.getValue());
+    }
+
+    private void clearEnvPropsTable() {
+        envPropsTable.getItems().forEach(property -> {
+            try {
+                Response response = HttpSimulationSetters.setEnvironmentProperty(creatingSimulation.getValue().getUuid(), property.getName(), null);
+
+                if (!response.isSuccessful())
+                    response.close();
+
+            } catch (Exception ignored) { }
+        });
+
+        envPropsTable.refresh();
+    }
+
+    @FXML
+    private void handleClear() {
+        if (isSimulationEmpty())
+            return;
+
+        populationTableController.clearPopulationTable();
+        clearEnvPropsTable();
+    }
+
+    @FXML
+    private void handleRun() {
+        if (isSimulationEmpty())
+            return;
+
+        if (populationTableController.validateAllInitialized()) {
+            try {
+                String simulationUuid = creatingSimulation.getValue().getUuid();
+                Response response = HttpSimulation.enqueueSimulation(simulationUuid);
+
+                if (!response.isSuccessful()) {
+                    response.close();
+                    return;
+                }
+
+                if (Animations.IS_ANIMATIONS_ON.getValue()) {
+                    TrayNotification tray = new TrayNotification("SUCCESS", "Simulation was added to queue manager", NotificationType.SUCCESS);
+                    Platform.runLater(() -> tray.showAndDismiss(Restrictions.ANIMATION_DURATION));
+                }
+
+                navbarController.handleResultsClicked();
+            } catch (Exception ignored) { }
+        } else
+            Alerts.showAlert("FAILURE",  "Please initialize at least one of the entities population to be positive", Alert.AlertType.ERROR);
     }
 }
